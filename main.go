c6082c04a24f0171fa7a494e7c7ccf1b7f2bfab6
Jake Champlin <jake.champlin.27@gmail.com>
2017-06-06 12:47:54 -0400

    Initial transfer of provider code

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
+import (
+	"github.com/hashicorp/terraform/plugin"
+	"github.com/terraform-providers/terraform-provider-vcd/vcd"
+)
+
 func main() {
-	/*plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: opc.Provider})*/
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: vcd.Provider})
 }
