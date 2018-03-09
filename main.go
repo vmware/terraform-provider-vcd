038a4695adb5d5c37781d553858fa845b5711872
Kristoffer Dalby <kradalby@kradalby.no>
2018-03-09 10:01:21 +0100

    renames terraform-provider to kradalby in fork

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/terraform-providers/terraform-provider-vcd/vcd"
+	"github.com/kradalby/terraform-provider-vcd/vcd"
 )
 
 func main() {
