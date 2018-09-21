7db2e99802659900312ec0292a89793437f542f1
Linas Virbalas <lvirbalas@vmware.com>
2018-09-21 18:22:41 +0300

    Update vcd_test.tf configuration example
    (Cherry picked by lvirbalas from https://github.com/auppunda/terraform-provider-vcd/commit/fdf2521)

--- ResourceExamples/vcd_test.tf
+++ ResourceExamples/vcd_test.tf
@@ -1,35 +1,71 @@
 
 provider "vcd" {
-  user                 = "administrator"
-  password             = "ca$hc0w"
-  org                  = "au"
-  url                  = "https://bos1-vcd-sp-static-200-22.eng.vmware.com/api"
-  vdc                  = "au-vdc"
+  user                 = "root"
+  password             = "root"
+  org                  = "System"
+  url                  = "https://api.vcd.com/api"
   max_retry_timeout    = "60"
   allow_unverified_ssl = "true"
 }
 
+resource "vcd_dnat" "web2" {
+
+  edge_gateway = "test_edge_3"
+  org = "au"
+  vdc = "au-vdc"
+  external_ip  = "10.x.x.x"
+  port         = 80
+  internal_ip  = "10.x.x.x"
+  translated_port = 8080
+}
+
+resource "vcd_snat" "outbound" {
+  edge_gateway = "test_edge_3"
+  org = "au"
+  vdc = "au-vdc"
+  external_ip  = "10.x.x.x"
+  internal_ip  = "10.x.x.x"
+}
+resource "vcd_network" "net" {
+  name         = "my-nt"
+  org = "au"
+  vdc = "au-vdc"
+  edge_gateway = "test_edge_3"
+  gateway      = "10.10.1.1"
+
+  dhcp_pool {
+    start_address = "10.10.1.2"
+    end_address   = "10.10.1.100"
+  }
+
+  static_ip_pool {
+    start_address = "10.10.1.152"
+    end_address   = "10.10.1.254"
+  }
+}
+
 resource "vcd_vapp" "test-tf-2" {
   name          = "test-tf-2"
+  org           = "au"
+  vdc           = "au-vdc"
+ 
 }
 
-resource "vcd_vapp_vm" "web" {
-	vapp_name = "test-tf-2"
-	name = "my-first-vm"
-	catalog_name = "Re"
-	template_name = "phot"
-	network_name = "testNetwork"
-  ip = "10.10.0.153"
-  accept_all_eulas = "true"
-
-	memory = 1024
-	cpus = 1
-
-}
-
-resource "vcd_org" "test"{
-  name = "test"
-  full_name = "test"
+resource "vcd_org" "test5" {
+  name = "test5"
+  full_name = "test5"
   is_enabled = "true"
-  network_name = "testNetwork"
+  stored_vm_quota = 10
+  deployed_vm_quota = 10
+  force = "true"
+  recursive = "true"
+}
+
+resource "vcd_org" "test4" {
+  name = "test4"
+  full_name = "test4"
+  is_enabled = "true"
+  stored_vm_quota = 10
+  force = "true"
+  recursive = "true"
 }
