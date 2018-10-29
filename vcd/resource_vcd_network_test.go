49ae49cc3c9c7fb6e4260e27918c2677883e0b79
Giuseppe Maxia <gmaxia@vmware.com>
2018-10-29 11:58:41 +0100

    Add OrgVDC network types routed, bridged, isolated
    * Deprecate current vcd_network, replaced by vcd_network_routed.
    * Implement vcd_network_routed, as an Org VDC network with mandatory Edge
      Gateway.
    * Implement vcd_network_direct, with a mandatory connection to an External
      network.
    * Implement vcd_network_isolated, with no external connections.
    * Implement tests for all the three network types.
    * Implement an optional method to add the provider to every Terraform snippet
      used for testing. (Triggered by env variable VCD_ADD_PROVIDER).
    * Update .gitignore with common test artifacts
    * Remove deprecated property maxRetryTimeout.
    * Add new network resources to vcd provider.
    * Remove explicit calls to vdc.Refresh prior to vdc.Find* calls, which include
     a refresh in go-vcloud-director code.
    * Replace vcd_network with vcd_network_routed in other tests.
    
    Signed-off-by: Giuseppe Maxia <gmaxia@vmware.com>

--- vcd/resource_vcd_network_test.go
+++ vcd/resource_vcd_network_test.go
@@ -12,47 +12,130 @@ import (
 	"github.com/vmware/go-vcloud-director/govcd"
 )
 
-func TestAccVcdNetwork_Basic(t *testing.T) {
-	var network govcd.OrgVDCNetwork
-	generatedHrefRegexp := regexp.MustCompile("^https://")
+type networkDef struct {
+	name            string
+	gateway         string
+	startIpAddress  string
+	endIpAddress    string
+	externalNetwork string
+	configText      string
+	resourceName    string
+}
+
+func TestAccVcdNetworkIsolated(t *testing.T) {
 	if vcdShortTest {
 		t.Skip(acceptanceTestsSkipped)
 		return
 	}
 
-	networkName := "TestAccVcdNetwork"
-	var params = StringMap{
-		"Org":         testConfig.VCD.Org,
-		"Vdc":         testConfig.VCD.Vdc,
-		"EdgeGateway": testConfig.Networking.EdgeGateway,
-		"NetworkName": networkName,
+	var def = networkDef{
+		name:           "TestAccVcdNetworkIso",
+		gateway:        "192.168.2.1",
+		startIpAddress: "192.168.2.2",
+		endIpAddress:   "192.168.2.100",
+		configText:     testAccCheckVcdNetworkIsolated,
+		resourceName:   "vcd_network_isolated",
 	}
 
-	configText := templateFill(testAccCheckVcdNetwork_basic, params)
+	runTest(def, t)
+}
+
+func TestAccVcdNetworkRouted(t *testing.T) {
+	if vcdShortTest {
+		t.Skip(acceptanceTestsSkipped)
+		return
+	}
+
+	var def = networkDef{
+		name:           "TestAccVcdNetworkEGW",
+		gateway:        "10.10.102.1",
+		startIpAddress: "10.10.102.2",
+		endIpAddress:   "10.10.102.100",
+		configText:     testAccCheckVcdNetworkRouted,
+		resourceName:   "vcd_network_routed",
+	}
+	runTest(def, t)
+}
+
+func TestAccVcdNetworkDirect(t *testing.T) {
+	if vcdShortTest {
+		t.Skip(acceptanceTestsSkipped)
+		return
+	}
+
+	var def = networkDef{
+		name:            "TestAccVcdNetworkDirect",
+		externalNetwork: testConfig.Networking.ExternalNetwork,
+		configText:      testAccCheckVcdNetworkDirect,
+		resourceName:    "vcd_network_direct",
+	}
+	runTest(def, t)
+}
+
+func runTest(def networkDef, t *testing.T) {
+
+	generatedHrefRegexp := regexp.MustCompile("^https://")
+
+	networkName := def.name
+	var params = StringMap{
+		"Org":             testConfig.VCD.Org,
+		"Vdc":             testConfig.VCD.Vdc,
+		"EdgeGateway":     testConfig.Networking.EdgeGateway,
+		"NetworkName":     networkName,
+		"Gateway":         def.gateway,
+		"StartIpAddress":  def.startIpAddress,
+		"EndIpAddress":    def.endIpAddress,
+		"ExternalNetwork": def.externalNetwork,
+		"FuncName":        networkName,
+	}
+	var network govcd.OrgVDCNetwork
+	configText := templateFill(def.configText, params)
 	if os.Getenv("GOVCD_DEBUG") != "" {
 		log.Printf("#[DEBUG] CONFIGURATION: %s", configText)
 	}
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckVcdNetworkDestroy,
-		Steps: []resource.TestStep{
+
+	// steps for external network
+	var steps = []resource.TestStep{
+		resource.TestStep{
+			Config:      configText,
+			ExpectError: regexp.MustCompile(`After applying this step, the plan was not empty`),
+			Check: resource.ComposeTestCheckFunc(
+				testAccCheckVcdNetworkExists(def.resourceName+"."+networkName, &network),
+				testAccCheckVcdNetworkAttributes(networkName, &network),
+				resource.TestCheckResourceAttr(
+					def.resourceName+"."+networkName, "name", networkName),
+				resource.TestMatchResourceAttr(
+					def.resourceName+"."+networkName, "href", generatedHrefRegexp),
+			),
+		},
+	}
+
+	// Basic tests for isolated and routed
+	if def.externalNetwork == "" {
+		steps = []resource.TestStep{
 			resource.TestStep{
 				Config: configText,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckVcdNetworkExists("vcd_network."+networkName, &network),
+					testAccCheckVcdNetworkExists(def.resourceName+"."+networkName, &network),
 					testAccCheckVcdNetworkAttributes(networkName, &network),
 					resource.TestCheckResourceAttr(
-						"vcd_network."+networkName, "name", networkName),
+						def.resourceName+"."+networkName, "name", networkName),
 					resource.TestCheckResourceAttr(
-						"vcd_network."+networkName, "static_ip_pool.#", "1"),
+						def.resourceName+"."+networkName, "static_ip_pool.#", "1"),
 					resource.TestCheckResourceAttr(
-						"vcd_network."+networkName, "gateway", "10.10.102.1"),
+						def.resourceName+"."+networkName, "gateway", def.gateway),
 					resource.TestMatchResourceAttr(
-						"vcd_network."+networkName, "href", generatedHrefRegexp),
+						def.resourceName+"."+networkName, "href", generatedHrefRegexp),
 				),
 			},
-		},
+		}
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: func(s *terraform.State) error { return testAccCheckVcdNetworkDestroy(s, def.resourceName) },
+		Steps:        steps,
 	})
 }
 
@@ -85,11 +168,23 @@ func testAccCheckVcdNetworkExists(n string, network *govcd.OrgVDCNetwork) resour
 	}
 }
 
-func testAccCheckVcdNetworkDestroy(s *terraform.State) error {
+func testAccCheckVcdNetworkIsolatedDestroy(s *terraform.State) error {
+	return testAccCheckVcdNetworkDestroy(s, "vcd_network_isolated")
+}
+
+func testAccCheckVcdNetworkDirectDestroy(s *terraform.State) error {
+	return testAccCheckVcdNetworkDestroy(s, "vcd_network_direct")
+}
+
+func testAccCheckVcdNetworkRoutedDestroy(s *terraform.State) error {
+	return testAccCheckVcdNetworkDestroy(s, "vcd_network_routed")
+}
+
+func testAccCheckVcdNetworkDestroy(s *terraform.State, networkType string) error {
 	conn := testAccProvider.Meta().(*VCDClient)
 
 	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "vcd_network" {
+		if rs.Type != networkType {
 			continue
 		}
 
@@ -101,7 +196,7 @@ func testAccCheckVcdNetworkDestroy(s *terraform.State) error {
 		_, err = vdc.FindVDCNetwork(rs.Primary.ID)
 
 		if err == nil {
-			return fmt.Errorf("network %s still exists.", rs.Primary.ID)
+			return fmt.Errorf("network %s still exists", rs.Primary.ID)
 		}
 
 		return nil
@@ -121,17 +216,41 @@ func testAccCheckVcdNetworkAttributes(name string, network *govcd.OrgVDCNetwork)
 	}
 }
 
-const testAccCheckVcdNetwork_basic = `
-resource "vcd_network" "{{.NetworkName}}" {
+const testAccCheckVcdNetworkIsolated = `
+resource "vcd_network_isolated" "{{.NetworkName}}" {
+  name       = "{{.NetworkName}}"
+  org        = "{{.Org}}"
+  vdc        = "{{.Vdc}}"
+  gateway    = "{{.Gateway}}"
+  dns1       = "192.168.2.1"
+
+  static_ip_pool {
+    start_address = "{{.StartIpAddress}}"
+    end_address   = "{{.EndIpAddress}}"
+  }
+}
+`
+
+const testAccCheckVcdNetworkDirect = `
+resource "vcd_network_direct" "{{.NetworkName}}" {
+  name             = "{{.NetworkName}}"
+  org              = "{{.Org}}"
+  vdc              = "{{.Vdc}}"
+  external_network = "{{.ExternalNetwork}}"
+}
+`
+
+const testAccCheckVcdNetworkRouted = `
+resource "vcd_network_routed" "{{.NetworkName}}" {
   name         = "{{.NetworkName}}"
   org          = "{{.Org}}"
   vdc          = "{{.Vdc}}"
   edge_gateway = "{{.EdgeGateway}}"
-  gateway      = "10.10.102.1"
+  gateway      = "{{.Gateway}}"
 
   static_ip_pool {
-    start_address = "10.10.102.2"
-    end_address   = "10.10.102.254"
+    start_address = "{{.StartIpAddress}}"
+    end_address   = "{{.EndIpAddress}}"
   }
 }
 `
