acb66f32f22719878050a61094d5dc89b51b902e
Vaidotas Bauzys <vbauzys@vmware.com>
2018-11-14 12:25:58 +0200

    Fix error "runtime: kevent on fd 4 failed with 9" when running test.

--- vcd/resource_vcd_catalog_item.go
+++ vcd/resource_vcd_catalog_item.go
@@ -1,6 +1,7 @@
 package vcd
 
 import (
+	"flag"
 	"fmt"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/vmware/go-vcloud-director/govcd"
@@ -91,7 +92,13 @@ func resourceVcdCatalogItemCreate(d *schema.ResourceData, meta interface{}) erro
 		return fmt.Errorf("error upload new catalog item: %#v", err)
 	}
 
-	terraformStdout := os.NewFile(uintptr(4), "stdout")
+	var terraformStdout *os.File
+	// Needed only to avoid error in tests - test fails miserable when uintptr(4) is used
+	if flag.Lookup("test.v") == nil {
+		terraformStdout = os.NewFile(uintptr(4), "stdout")
+	} else {
+		terraformStdout = os.Stdin
+	}
 
 	if d.Get("show_upload_progress").(bool) {
 		for {
