d3bf06ace44a04da0bd08df09502bad73b565dfd
Vaidotas Bauzys <vbauzys@vmware.com>
2019-03-13 17:08:17 +0200

    Add independent disk resource implementation and attach disk possibility
    Signed-off-by: Vaidotas Bauzys <vbauzys@vmware.com>

---
+++ vcd/resource_vcd_independent_disk_test.go
@@ -0,0 +1,109 @@
+package vcd
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/vmware/go-vcloud-director/v2/govcd"
+)
+
+var resourceName = "TestAccVcdIndependentDiskBasic_1"
+var name = "TestAccVcdIndependentDiskBasic"
+
+func TestAccVcdIndependentDiskBasic(t *testing.T) {
+	var params = StringMap{
+		"Org":                testConfig.VCD.Org,
+		"Vdc":                testConfig.VCD.Vdc,
+		"name":               name,
+		"size":               "5",
+		"busType":            "SCSI",
+		"busSubType":         "lsilogicsas",
+		"storageProfileName": "*",
+		"ResourceName":       resourceName,
+	}
+
+	if vcdShortTest {
+		t.Skip(acceptanceTestsSkipped)
+		return
+	}
+	configText := templateFill(testAccCheckVcdIndependentDiskBasic, params)
+
+	fmt.Printf("#[DEBUG] CONFIGURATION: %s", configText)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testDiskResourcesDestroyed,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: configText,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDiskCreated("vcd_independent_disk." + resourceName),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckDiskCreated(itemName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		injectItemRs, ok := s.RootModule().Resources[itemName]
+		if !ok {
+			return fmt.Errorf("not found: %s", itemName)
+		}
+
+		if injectItemRs.Primary.ID == "" {
+			return fmt.Errorf("no disk insert ID is set")
+		}
+
+		conn := testAccProvider.Meta().(*VCDClient)
+
+		_, vdc, err := conn.GetOrgAndVdc(testConfig.VCD.Org, testConfig.VCD.Vdc)
+		if err != nil {
+			return fmt.Errorf(errorRetrievingVdcFromOrg, testConfig.VCD.Vdc, testConfig.VCD.Org, err)
+		}
+
+		disk, _ := vdc.QueryDisk(name)
+		if disk == (govcd.DiskRecord{}) {
+			return fmt.Errorf("independent disk %s isn't exist and error: %#v", itemName, err)
+		}
+
+		return nil
+	}
+}
+
+func testDiskResourcesDestroyed(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*VCDClient)
+	for _, rs := range s.RootModule().Resources {
+		itemName := rs.Primary.Attributes["name"]
+		if rs.Type != "vcd_independent_disk" && itemName != name {
+			continue
+		}
+
+		_, vdc, err := conn.GetOrgAndVdc(testConfig.VCD.Org, testConfig.VCD.Vdc)
+		if err != nil {
+			return fmt.Errorf(errorRetrievingVdcFromOrg, testConfig.VCD.Vdc, testConfig.VCD.Org, err)
+		}
+
+		_, err = vdc.QueryDisk(name)
+		if err != nil && !strings.Contains(err.Error(), "found results") {
+			return fmt.Errorf("independent disk %s still exist and error: %#v", itemName, err)
+		}
+	}
+	return nil
+}
+
+const testAccCheckVcdIndependentDiskBasic = `
+resource "vcd_independent_disk" "{{.ResourceName}}" {
+  org             = "{{.Org}}"
+  vdc             = "{{.Vdc}}"
+  name            = "{{.name}}"
+  size            = "{{.size}}"
+  bus_type        = "{{.busType}}"
+  bus_sub_type    = "{{.busSubType}}"
+  storage_profile = "{{.storageProfileName}}"
+}
+`
---
+++ website/docs/r/independent_disk.html.markdown
@@ -0,0 +1,40 @@
+---
+layout: "vcd"
+page_title: "vCloudDirector: vcd_independent_disk"
+sidebar_current: "docs-vcd-independent-disk"
+description: |-
+  Provides a vCloud Director independent disk resource. This can be used to create and delete independent disks.
+---
+
+# vcd\_inserted\_media
+
+Provides a vCloud Director independent disk resource. This can be used to create and delete independent disks.
+
+Supported in provider *v2.1+*
+
+## Example Usage
+
+```
+resource "vcd_independent_disk" "myNewIndependentDisk" {
+  org             = "my-org"
+  vdc             = "my-vcd"
+  
+  name            = "logDisk"
+  size            = "33"
+  bus_type        = "SCSI"
+  bus_sub_type    = "VirtualSCSI"
+  storage_profile = "external"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `org` - (Optional; *v2.0+*) The name of organization to use, optional if defined at provider level. Useful when connected as sysadmin working across different organisations
+* `vdc` - (Optional; *v2.0+*) The name of VDC to use, optional if defined at provider level
+* `name` - (Required) Disk name
+* `size` - (Required) - Size of disk in GB
+* `bus_type` - (Optional) - Disk bus type. Values can be: IDE, SCSI, SATA 
+* `bus_sub_type` - (Optional) - Disk bus subtype. Values can be: "IDE" for IDE. buslogic, lsilogic, lsilogicsas, VirtualSCSI for SCSI and ahci for SATA
+* `storage_profile` - (Optional) - The name of storage profile where disk will be created
