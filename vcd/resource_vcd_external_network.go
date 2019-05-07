3359c3feeea116386233a176ff764491cdf6796d
Vaidotas Bauzys <vbauzys@vmware.com>
2019-05-07 12:24:32 +0300

    Improve error message
    Signed-off-by: Vaidotas Bauzys <vbauzys@vmware.com>

--- vcd/resource_vcd_external_network.go
+++ vcd/resource_vcd_external_network.go
@@ -326,7 +326,7 @@ func GetVcenterHref(vcdClient *govcd.VCDClient, name string) (string, error) {
 		return "", err
 	}
 	if len(virtualCenters) == 0 || len(virtualCenters) > 1 {
-		return "", fmt.Errorf("not one: %d, vSphere server found with name '%s'", len(virtualCenters), name)
+		return "", fmt.Errorf("vSphere server found %d instances with name '%s'", len(virtualCenters), name)
 	}
 	return virtualCenters[0].HREF, nil
 }
