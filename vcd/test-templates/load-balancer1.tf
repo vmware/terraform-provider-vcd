b0abaacb3302ce9104d264a7fa1719e3862ff955
Dainius S <dserplis@vmware.com>
2019-06-18 22:19:54 +0300

    Update test template

--- vcd/test-templates/load-balancer1.tf
+++ vcd/test-templates/load-balancer1.tf
@@ -2,13 +2,12 @@
 ## apply-options -parallelism=1
 ## destroy-options -parallelism=1
 
-# Edge gateway load balancer configuration. It will get more details, but for now is used to
-# make sure that locks work fine and it does not fail
+# Edge gateway load balancer configuration from separate components
 
 # v2.4.0+
 
 variable "service_monitor_count" {
-  default = 20
+  default = 10
 }
 
 resource "vcd_lb_service_monitor" "test" {
@@ -34,7 +33,6 @@ resource "vcd_lb_service_monitor" "test" {
   }
 }
 
-
 data "vcd_lb_service_monitor" "ds-lb" {
   count = "${var.service_monitor_count}"
 
@@ -42,4 +40,72 @@ data "vcd_lb_service_monitor" "ds-lb" {
   vdc          = "{{.Vdc}}"
   edge_gateway = "{{.EdgeGateway}}"
   name         = "${vcd_lb_service_monitor.test[count.index].name}"
+}
+
+
+resource "vcd_lb_server_pool" "server-pool" {
+  count = "${var.service_monitor_count}"
+
+  org          = "{{.Org}}"
+  vdc          = "{{.Vdc}}"
+  edge_gateway = "{{.EdgeGateway}}"
+
+  name = "test-pool-${count.index}"
+  algorithm = "round-robin"
+  enable_transparency = "true"
+
+  monitor_id = "${vcd_lb_service_monitor.test[count.index].id}"
+
+  member {
+    condition = "enabled"
+    name = "member1"
+    ip_address = "1.1.1.1"
+    port = 8443
+    monitor_port = 9000
+    weight = 1
+    min_connections = 0
+    max_connections = 100
+  }
+
+  member {
+    condition = "drain"
+    name = "member2"
+    ip_address = "2.2.2.2"
+    port = 7000
+    monitor_port = 4000
+    weight = 2
+    min_connections = 6
+    max_connections = 8
+  }
+
+  member {
+    condition = "disabled"
+    name = "member3"
+    ip_address = "3.3.3.3"
+    port = 3333
+    monitor_port = 4444
+    weight = 6
+    min_connections = 3
+    max_connections = 3
+  }
+
+  member {
+    condition = "disabled"
+    name = "member4"
+    ip_address = "4.4.4.4"
+    port = 3333
+    monitor_port = 4444
+    weight = 6
+  }
+}
+
+
+
+data "vcd_lb_server_pool" "ds-pool" {
+  count = "${var.service_monitor_count}"
+
+  org          = "{{.Org}}"
+  vdc          = "{{.Vdc}}"
+  edge_gateway = "{{.EdgeGateway}}"
+  name         = "${vcd_lb_server_pool.server-pool[count.index].name}"
 }
