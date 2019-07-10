2ed706cf81f5541b14921cdeeaa50be7d0669f25
Giuseppe Maxia <gmaxia@vmware.com>
2019-07-10 12:20:59 +0200

    merge tags in provider_test.go

--- vcd/provider_test.go
+++ vcd/provider_test.go
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-// +build api functional catalog vapp network extnetwork org query vm vdc gateway disk binary lb lbServiceMonitor lbServerPool user ALL
-=======
-// +build api functional catalog vapp network extnetwork org query vm vdc gateway disk binary lb lbAppProfile lbServiceMonitor lbServerPool ALL
->>>>>>> master
+// +build api functional catalog vapp network extnetwork org query vm vdc gateway disk binary lb lbAppProfile lbServiceMonitor lbServerPool user ALL
 
 package vcd
 
