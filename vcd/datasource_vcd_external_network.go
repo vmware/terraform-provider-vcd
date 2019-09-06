d886b131377908b0959cd4614e428ebfee7be8e4
Giuseppe Maxia <dataclouder@users.noreply.github.com>
2019-09-06 16:37:37 +0200

    Add datasource external-network (#322)
    * Update vendor directory
    * Add datasource and import for external network
    - Fix incomplete read function for external network resource
    - Move type StringMap from config_test.go to config.go
    - Add data source for external network
    - Add import for external network resource
    - Add documentation
    - Update CHANGELOG.md
    - Improve external network resource tests
    - Modify TestAccVcdDNAT_WithExtNetw test to include external network data source
    * Fix DNS spelling in descriptions
    * Apply temporary workaround for git.apache.org outage
    * Fix external network example in docs

---
+++ vcd/datasource_vcd_external_network.go
@@ -0,0 +1,103 @@
+package vcd
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func datasourceVcdExternalNetwork() *schema.Resource {
+	return &schema.Resource{
+		Read: resourceVcdExternalNetworkRead,
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"ip_scope": &schema.Schema{
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "A list of IP scopes for the network",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"gateway": &schema.Schema{
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Gateway of the network",
+						},
+						"netmask": &schema.Schema{
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Network mask",
+						},
+						"dns1": &schema.Schema{
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Primary DNS server",
+						},
+						"dns2": &schema.Schema{
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Secondary DNS server",
+						},
+						"dns_suffix": &schema.Schema{
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "DNS suffix",
+						},
+						"static_ip_pool": &schema.Schema{
+							Type:        schema.TypeList,
+							Computed:    true,
+							Description: "IP ranges used for static pool allocation in the network",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"start_address": &schema.Schema{
+										Type:        schema.TypeString,
+										Computed:    true,
+										Description: "Start address of the IP range",
+									},
+									"end_address": &schema.Schema{
+										Type:        schema.TypeString,
+										Computed:    true,
+										Description: "End address of the IP range",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			"vsphere_network": &schema.Schema{
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "A list of port groups that back this network. Each referenced DV_PORTGROUP or NETWORK must exist on a vCenter server registered with the system.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"vcenter": &schema.Schema{
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The vCenter server name",
+						},
+						"name": &schema.Schema{
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The name of the port group",
+						},
+						"type": &schema.Schema{
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The vSphere port group type. One of: DV_PORTGROUP (distributed virtual port group), NETWORK",
+						},
+					},
+				},
+			},
+			"retain_net_info_across_deployments": &schema.Schema{
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "Specifies whether the network resources such as IP/MAC of router will be retained across deployments. Default is false.",
+			},
+		},
+	}
+}
