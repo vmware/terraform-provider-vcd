43258218d262af63e03cc6270e8a1ddaec04b774
Vaidotas Bauzys <vbauzys@vmware.com>
2019-09-12 14:38:03 +0300

    Move test
    Signed-off-by: Vaidotas Bauzys <vbauzys@vmware.com>

--- vcd/datasource_vcd_org_vdc_test.go
+++ vcd/datasource_vcd_org_vdc_test.go
@@ -127,54 +127,6 @@ func testAccDataSourceVcdOrgVdc(name, vdcName string) resource.TestCheckFunc {
 	}
 }
 
-func TestGetHashValuesFromKey(t *testing.T) {
-
-	type testInfo struct {
-		key      string
-		parent   string
-		child    string
-		expected []string
-	}
-	var testData = []testInfo{
-		{
-			"first.1234.second.5678.third",
-			"first",
-			"second",
-			[]string{"1234", "5678"},
-		},
-		{
-			"compute_capacity.315866465.memory.508945747.limit",
-			"compute_capacity",
-			"memory",
-			[]string{"315866465", "508945747"},
-		},
-		{
-			"compute_capacity.315866465.cpu.798465156.limit",
-			"compute_capacity",
-			"cpu",
-			[]string{"315866465", "798465156"},
-		},
-	}
-	for _, td := range testData {
-		testMap := map[string]string{
-			td.key: "",
-		}
-		first, second, err := getHashValuesFromKey(testMap, td.parent, td.child)
-		if err != nil {
-			t.Logf("processing key '%s' got error %s", td.key, err)
-			t.Fail()
-		}
-		if first != td.expected[0] {
-			t.Logf("Expected result from key '%s' was '%s' - Got '%s' instead", td.key, td.parent, td.expected[0])
-			t.Fail()
-		}
-		if second != td.expected[1] {
-			t.Logf("Expected result from key '%s' was '%s' - Got '%s' instead", td.key, td.child, td.expected[1])
-			t.Fail()
-		}
-	}
-}
-
 const testAccCheckVcdVdcDatasource_basic = `
 data "vcd_org_vdc" "existingVdc" {
   org  = "{{.OrgName}}"
