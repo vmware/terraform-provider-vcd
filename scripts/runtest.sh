7baf9f215f7d4b1956aadf6c2f56c8b4e0747c3a
Giuseppe Maxia <gmaxia@vmware.com>
2019-11-08 15:05:31 +0100

    Add staticcheck download to Travis ops

--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -196,15 +196,48 @@ function exists_in_path {
 
 function check_static {
     static_check=$(exists_in_path staticcheck)
+    if [  -z "$staticcheck" -a -n "$TRAVIS" ]
+    then
+        # Variables found in staticcheck-config.sh
+        # STATICCHECK_URL
+        # STATICCHECK_VERSION
+        # STATICCHECK_FILE
+        if [ -f $scripts_dir/staticcheck-config.sh ]
+        then
+            source $scripts_dir/staticcheck-config.sh
+        else
+            echo "File $scripts_dir/staticcheck-config.sh not found - Skipping check"
+            exit 0
+        fi
+        download_name=$STATICCHECK_URL/$STATICCHECK_VERSION/$STATICCHECK_FILE
+        wget=$(exists_in_path wget)
+        if [ -z "$wget" ]
+        then
+            echo "'wget' executable not found - Skipping check"
+            exit 0
+        fi
+        $wget $download_name
+        if [ -n "$STATICCHECK_FILE" ]
+        then
+            tar -xzf $STATICCHECK_FILE
+            executable=$PWD/staticcheck/staticcheck
+            if [ ! -f $executable ]
+            then
+                echo "Extracted executable not available - Skipping check"
+            fi
+            chmod +x $executable
+            static_check=$executable
+        fi
+    fi
 
     if [ -n "$static_check" ]
     then
         echo "## Found $static_check"
         echo -n "## "
-        staticcheck -version
+        $static_check -version
         echo -n "## Checking "
         pwd
-        staticcheck -tags functional .
+        $static_check -tags functional .
         exit_code=$?
         if [ "$exit_code" != "0" ]
         then
---
+++ scripts/staticcheck-config.sh
@@ -0,0 +1,4 @@
+export STATICCHECK_URL=https://github.com/dominikh/go-tools/releases/download
+export STATICCHECK_VERSION=2019.2.3
+export STATICCHECK_FILE=staticcheck_linux_amd64.tar.gz
+
