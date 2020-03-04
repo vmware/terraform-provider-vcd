aa23587d6096185a76c70cca2639e2e8b3918120
Vaidotas Bauzys <vbauzys@vmware.com>
2020-03-04 14:22:21 +0200

    Added warning for VM
    Signed-off-by: Vaidotas Bauzys <vbauzys@vmware.com>

--- vcd/resource_vcd_vapp_vm.go
+++ vcd/resource_vcd_vapp_vm.go
@@ -589,6 +589,9 @@ func addVdcNetwork(networkNameToAdd string, vdc *govcd.Vdc, vapp govcd.VApp, vcd
 	}
 
 	if !isAlreadyVappNetwork {
+		// TODO remove when major release is done
+		_, _ = fmt.Fprintf(getTerraformStdout(), "DEPRECATED: attaching an Org network `%s` to a vApp `%s` through VM's network block alone is deprecated. "+
+			"Network should be first attached to a vApp by creating a vcd_vapp_org_network resource and only then referenced in the network block. \n", networkNameToAdd, vapp.VApp.Name)
 		task, err := vapp.AddRAWNetworkConfig([]*types.OrgVDCNetwork{vdcNetwork})
 		if err != nil {
 			return &types.OrgVDCNetwork{}, fmt.Errorf("error assigning network to vApp: %s", err)
