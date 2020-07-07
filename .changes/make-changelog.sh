672a625d5bba4c5476f04e4dd7c9dd1158ee960f
Giuseppe Maxia <gmaxia@vmware.com>
2020-07-07 14:06:20 +0200

    [skip ci] Remove unneeded comment

--- .changes/make-changelog.sh
+++ .changes/make-changelog.sh
@@ -22,6 +22,3 @@ do
     echo ""
 done
 
-
-#484-improvements.md   487-improvements.md   499-improvements.md   504-features.md       511-bugfixes.md       512-bugfixes.md       514-features.md       518-features.md       522-bugfixes.md
-# 484-improvements.md~  492-notes.md          501-improvements.md   505-improvements.md   511-features.md       513-features.md       518-deprecations.md   520-features.md       all.md
