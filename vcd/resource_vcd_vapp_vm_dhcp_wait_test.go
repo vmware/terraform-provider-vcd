179f2ccd834ff4ba911903e15f23ba211c24d6c9
Vaidotas Bauzys <vbauzys@vmware.com>
2020-07-09 14:29:41 +0300

    improvements

--- vcd/resource_vcd_vapp_vm_dhcp_wait_test.go
+++ vcd/resource_vcd_vapp_vm_dhcp_wait_test.go
@@ -159,7 +159,7 @@ resource "vcd_vapp_vm" "{{.VMName}}" {
   network {
     type               = "none"
     ip_allocation_mode = "NONE"
-    type               = "none"
+    connect            = "false"
   }
 }
 
