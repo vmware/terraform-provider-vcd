1f587d70057a997d9fba61d39adec32edc772bb3
Giuseppe Maxia <gmaxia@vmware.com>
2020-10-09 10:47:10 +0200

    Remove redundant terraform block from inline tests

--- vcd/config_test.go
+++ vcd/config_test.go
@@ -207,15 +207,6 @@ const (
 # file {{.CallerFileName}}
 #
 
-terraform {
-  required_providers {
-    vcd = {
-      source = "vmware/vcd"
-    }
-  }
-  # required_version = ">= 0.13"
-}
-
 provider "vcd" {
   user                 = "{{.User}}"
   password             = "{{.Password}}"
