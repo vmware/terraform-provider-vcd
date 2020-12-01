cb3c40d6f2a6fddb2db241233d06d36e1f770ba4
Dainius S <dserplis@vmware.com>
2020-12-01 22:04:52 +0200

    Add vcd_storage_profile datasource

---
+++ vcd/datasource_vcd_storage_profile_test.go
@@ -0,0 +1,48 @@
+// +build catalog ALL functional
+
+package vcd
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccVcdStorageProfile(t *testing.T) {
+
+	var params = StringMap{
+		"Org":                testConfig.VCD.Org,
+		"Vdc":                testConfig.VCD.Vdc,
+		"StorageProfileName": testConfig.VCD.ProviderVdc.StorageProfile,
+		"Tags":               "catalog",
+	}
+
+	configText := templateFill(testAccVcdStorageProfile, params)
+	if vcdShortTest {
+		t.Skip(acceptanceTestsSkipped)
+		return
+	}
+	debugPrintf("#[DEBUG] CONFIGURATION: %s", configText)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { preRunChecks(t) },
+		ProviderFactories: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: configText,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr("data.vcd_storage_profile.sp", "id", regexp.MustCompile(`^urn:vcloud:vdcstorageProfile:.*`)),
+				),
+			},
+		},
+	})
+}
+
+const testAccVcdStorageProfile = `
+data "vcd_storage_profile" "sp" {
+  org  = "{{.Org}}"
+  vdc  = "{{.Vdc}}"
+  name = "{{.StorageProfileName}}"
+}
+`
