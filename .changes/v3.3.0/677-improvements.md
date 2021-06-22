1a40fea19f799157de2749c264e07e56c3e29b66
Giuseppe Maxia <dataclouder@users.noreply.github.com>
2021-06-22 14:37:22 +0200

    Roles management (#677)
    * Add role management resources and data sources
    * vcd_right (only data source)
    * vcd_role
    * vcd_global_role
    * vcd_rights_bundle
    * Add documentation on role management
    
    Signed-off-by: Giuseppe Maxia <gmaxia@vmware.com>

---
+++ .changes/v3.3.0/677-improvements.md
@@ -0,0 +1,1 @@
+* `datasource/vcd_resource_list` adds `vcd_right`, `vcd_role`, `vcd_global_role`, `vcd_rights_bundle` to the supported resource types [#677]
---
+++ vcd/resource_vcd_global_role_test.go
@@ -0,0 +1,151 @@
+// +build role ALL functional
+
+package vcd
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccVcdGlobalRole(t *testing.T) {
+	preTestChecks(t)
+	var globalRoleName = t.Name()
+	var globalRoleUpdateName = t.Name() + "-update"
+	var globalRoleDescription = "A long description containing some text."
+	var globalRoleUpdateDescription = "A shorter description."
+
+	var params = StringMap{
+		"Tenant":                      testConfig.VCD.Org,
+		"GlobalRoleName":              globalRoleName,
+		"GlobalRoleUpdateName":        globalRoleUpdateName,
+		"GlobalRoleDescription":       globalRoleDescription,
+		"GlobalRoleUpdateDescription": globalRoleUpdateDescription,
+		"FuncName":                    globalRoleName,
+		"Tags":                        "role",
+	}
+	configText := templateFill(testAccGlobalRole, params)
+
+	params["FuncName"] = globalRoleUpdateName
+	params["GlobalRoleDescription"] = globalRoleUpdateDescription
+	configTextUpdate := templateFill(testAccGlobalRoleUpdate, params)
+	if vcdShortTest {
+		t.Skip(acceptanceTestsSkipped)
+		return
+	}
+	debugPrintf("#[DEBUG] CONFIGURATION basic: %s\n", configText)
+	debugPrintf("#[DEBUG] CONFIGURATION update: %s\n", configTextUpdate)
+
+	resourceDef := "vcd_global_role." + globalRoleName
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviders,
+		CheckDestroy:      testAccCheckGlobalRoleDestroy(resourceDef),
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: configText,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGlobalRoleExists(resourceDef),
+					resource.TestCheckResourceAttr(resourceDef, "name", globalRoleName),
+					resource.TestCheckResourceAttr(resourceDef, "description", globalRoleDescription),
+					resource.TestCheckResourceAttr(resourceDef, "publish_to_all_tenants", "false"),
+					resource.TestCheckResourceAttr(resourceDef, "rights.#", "6"),
+					resource.TestCheckResourceAttr(resourceDef, "tenants.#", "1"),
+				),
+			},
+			resource.TestStep{
+				Config: configTextUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckGlobalRoleExists(resourceDef),
+					resource.TestCheckResourceAttr(resourceDef, "name", globalRoleUpdateName),
+					resource.TestCheckResourceAttr(resourceDef, "description", globalRoleUpdateDescription),
+					resource.TestCheckResourceAttr(resourceDef, "publish_to_all_tenants", "true"),
+					resource.TestCheckResourceAttr(resourceDef, "rights.#", "5"),
+				),
+			},
+			resource.TestStep{
+				ResourceName:      resourceDef,
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateIdFunc: importStateIdTopHierarchy(globalRoleUpdateName),
+			},
+		},
+	})
+	postTestChecks(t)
+}
+
+func testAccCheckGlobalRoleExists(identifier string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[identifier]
+		if !ok {
+			return fmt.Errorf("not found: %s", identifier)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no role ID is set")
+		}
+
+		conn := testAccProvider.Meta().(*VCDClient)
+
+		_, err := conn.Client.GetGlobalRoleById(rs.Primary.ID)
+		return err
+	}
+}
+
+func testAccCheckGlobalRoleDestroy(identifier string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[identifier]
+		if !ok {
+			return fmt.Errorf("not found: %s", identifier)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no role ID is set")
+		}
+
+		conn := testAccProvider.Meta().(*VCDClient)
+
+		_, err := conn.Client.GetGlobalRoleById(rs.Primary.ID)
+
+		if err == nil {
+			return fmt.Errorf("%s not deleted yet", identifier)
+		}
+		return nil
+
+	}
+}
+
+const testAccGlobalRole = `
+resource "vcd_global_role" "{{.GlobalRoleName}}" {
+  name        = "{{.GlobalRoleName}}"
+  description = "{{.GlobalRoleDescription}}"
+  rights = [
+    "Catalog: Add vApp from My Cloud",
+    "Catalog: Edit Properties",
+    "Catalog: View Private and Shared Catalogs",
+    "Organization vDC Compute Policy: View",
+    "vApp Template / Media: Edit",
+    "vApp Template / Media: View",
+  ]
+  publish_to_all_tenants = false
+  tenants                = [ "{{.Tenant}}" ]
+}
+`
+
+const testAccGlobalRoleUpdate = `
+resource "vcd_global_role" "{{.GlobalRoleName}}" {
+  name        = "{{.GlobalRoleUpdateName}}"
+  description = "{{.GlobalRoleUpdateDescription}}"
+  rights = [
+    # "Catalog: Add vApp from My Cloud",
+    "Catalog: Edit Properties",
+    "Catalog: View Private and Shared Catalogs",
+    "Organization vDC Compute Policy: View",
+    "vApp Template / Media: Edit",
+    "vApp Template / Media: View",
+  ]
+  publish_to_all_tenants = true
+}
+`
