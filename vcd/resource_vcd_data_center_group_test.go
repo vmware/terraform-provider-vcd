4792bb675c4624d74d04cda5d670ad17b2f96bba
Vaidotas Bauzys <vbauzys@vmware.com>
2021-12-02 13:41:57 +0200

    add test changes which allows to run test as org user
    Signed-off-by: Vaidotas Bauzys <vbauzys@vmware.com>

--- vcd/resource_vcd_data_center_group_test.go
+++ vcd/resource_vcd_data_center_group_test.go
@@ -4,6 +4,8 @@
 package vcd
 
 import (
+	"github.com/vmware/go-vcloud-director/v2/types/v56"
+	"log"
 	"regexp"
 	"testing"
 
@@ -26,9 +28,8 @@ func TestAccVcdDataCenterGroupResource(t *testing.T) {
 		t.Skip(t.Name() + " requires at least API v35.0 (vCD 10.2+)")
 	}
 
-	// TODO !!! run tests not as Sys Admin
 	if !vcdClient.Client.IsSysAdmin {
-		t.Skip(t.Name() + " only System Administrator can create Data center group")
+		t.Skip(t.Name() + " only System Administrator can run test of Data center group")
 	}
 
 	if testConfig.Nsxt.Vdc == "" || testConfig.VCD.NsxtProviderVdc.Name == "" ||
@@ -40,18 +41,151 @@ func TestAccVcdDataCenterGroupResource(t *testing.T) {
 	// String map to fill the template
 	var params = StringMap{
 		"Org":                       testConfig.VCD.Org,
+		"VDC":                       testConfig.Nsxt.Vdc,
 		"Name":                      "TestAccVcdDataCenterGroupResource",
 		"NameUpdated":               "TestAccVcdDataCenterGroupResourceUpdated",
 		"Description":               "myDescription",
 		"DescriptionUpdate":         "myDescription updated",
-		"StartingVdcName":           testConfig.Nsxt.Vdc,
 		"ProviderVdc":               testConfig.VCD.NsxtProviderVdc.Name,
 		"NetworkPool":               testConfig.VCD.NsxtProviderVdc.NetworkPool,
 		"Allocated":                 "1024",
 		"Limit":                     "1024",
 		"ProviderVdcStorageProfile": testConfig.VCD.ProviderVdc.StorageProfile,
+		"OrgUser":                   testConfig.TestEnvBuild.OrgUser,
+		"OrgUserPassword":           testConfig.TestEnvBuild.OrgUserPassword,
+		"VcdUrl":                    testConfig.Provider.Url,
+		"OrgUserProvider":           "",
 	}
 
+	runDataCenterGroupTest(t, params)
+}
+
+// TestAccVcdDataCenterGroupResourceAsOrgUser tests that data center group can be managed by Org user
+func TestAccVcdDataCenterGroupResourceAsOrgUser(t *testing.T) {
+	preTestChecks(t)
+
+	// This test requires access to the vCD before filling templates
+	// Thus it won't run in the short test
+	if vcdShortTest {
+		t.Skip(acceptanceTestsSkipped)
+		return
+	}
+
+	vcdClient := createTemporaryVCDConnection()
+	if vcdClient.Client.APIVCDMaxVersionIs("< 35.0") {
+		t.Skip(t.Name() + " requires at least API v35.0 (vCD 10.2+)")
+	}
+
+	if !vcdClient.Client.IsSysAdmin {
+		t.Skip(t.Name() + " only System Administrator can run test of Data center group")
+	}
+
+	if testConfig.Nsxt.Vdc == "" || testConfig.VCD.NsxtProviderVdc.Name == "" ||
+		testConfig.VCD.NsxtProviderVdc.NetworkPool == "" || testConfig.VCD.ProviderVdc.StorageProfile == "" {
+		t.Skip("Variables Nsxt.Vdc, VCD.NsxtProviderVdc.NetworkPool, VCD.NsxtProviderVdc.Name," +
+			" VCD.ProviderVdc.StorageProfile  must be set")
+	}
+
+	// Add needed right for Org User
+	rightsToAdd := addRights(t, vcdClient)
+
+	//remove added rights at the end of test
+	defer cleanupRightsAndBundle(t, vcdClient, rightsToAdd)
+
+	// String map to fill the template
+	var params = StringMap{
+		"Org":                       testConfig.VCD.Org,
+		"VDC":                       testConfig.Nsxt.Vdc,
+		"Name":                      "TestAccVcdDataCenterGroupResource",
+		"NameUpdated":               "TestAccVcdDataCenterGroupResourceUpdated",
+		"Description":               "myDescription",
+		"DescriptionUpdate":         "myDescription updated",
+		"ProviderVdc":               testConfig.VCD.NsxtProviderVdc.Name,
+		"NetworkPool":               testConfig.VCD.NsxtProviderVdc.NetworkPool,
+		"Allocated":                 "1024",
+		"Limit":                     "1024",
+		"ProviderVdcStorageProfile": testConfig.VCD.ProviderVdc.StorageProfile,
+		"OrgUser":                   testConfig.TestEnvBuild.OrgUser,
+		"OrgUserPassword":           testConfig.TestEnvBuild.OrgUserPassword,
+		"VcdUrl":                    testConfig.Provider.Url,
+		"OrgUserProvider":           "provider = vcd.orguser",
+	}
+
+	// run as Org user
+	runDataCenterGroupTest(t, params)
+}
+
+func addRights(t *testing.T, vcdClient *VCDClient) []types.OpenApiReference {
+	// add needed rights
+	var rightsToAdd []types.OpenApiReference
+	defaultRightsBundle, err := vcdClient.Client.GetRightsBundleByName("Default Rights Bundle")
+	if err != nil {
+		t.Errorf("%s error fetch default rights bundle: %s", t.Name(), err)
+	}
+
+	rightsBeforeChange, err := defaultRightsBundle.GetRights(nil)
+	if err != nil {
+		t.Errorf("%s error fetching rights: %s", t.Name(), err)
+	}
+
+	for _, rightName := range []string{
+		"vDC Group: Configure",
+		"vDC Group: Configure Logging",
+		"vDC Group: View",
+		"Organization vDC Distributed Firewall: Enable/Disable",
+		//"Security Tag Edit", 10.2 doesn't have it and for this kind'a testing not needed
+	} {
+		newRight, err := vcdClient.Client.GetRightByName(rightName)
+		if err != nil {
+			t.Errorf("%s error fetching right %s: %s", t.Name(), rightName, err)
+		}
+		foundRight := false
+		for _, old := range rightsBeforeChange {
+			if old.Name == rightName {
+				foundRight = true
+			}
+		}
+		if foundRight {
+			log.Printf("Right %s already in Default Rights Bundle\n", rightName)
+			// ignore
+		} else {
+			rightsToAdd = append(rightsToAdd, types.OpenApiReference{Name: newRight.Name, ID: newRight.ID})
+		}
+	}
+
+	if len(rightsToAdd) > 0 {
+		err = defaultRightsBundle.AddRights(rightsToAdd)
+		if err != nil {
+			t.Errorf("%s error adding rights %s: %s", t.Name(), rightsToAdd, err)
+		}
+		err = defaultRightsBundle.PublishAllTenants()
+		if err != nil {
+			t.Errorf("%s error publishing to tenants: %s", t.Name(), err)
+		}
+	}
+	return rightsToAdd
+}
+
+func cleanupRightsAndBundle(t *testing.T, vcdClient *VCDClient, rightsToRemove []types.OpenApiReference) {
+	if len(rightsToRemove) > 0 {
+		defaultRightsBundle, err := vcdClient.Client.GetRightsBundleByName("Default Rights Bundle")
+		if err != nil {
+			t.Errorf("%s error fetch default rights bundle: %s", t.Name(), err)
+		}
+		err = defaultRightsBundle.RemoveRights(rightsToRemove)
+		if err != nil {
+			t.Errorf("%s error removing rights %s: %s", t.Name(), rightsToRemove, err)
+		}
+		err = defaultRightsBundle.UnpublishAllTenants()
+		if err != nil {
+			t.Errorf("%s error unpublishing to tenants: %s", t.Name(), err)
+		}
+	}
+}
+
+func runDataCenterGroupTest(t *testing.T, params StringMap) {
+
+	params["FuncName"] = t.Name()
 	configText1 := templateFill(testAccVcdDataCenterGroupResource, params)
 	debugPrintf("#[DEBUG] CONFIGURATION for step 1: %s", configText1)
 
@@ -110,6 +244,21 @@ func TestAccVcdDataCenterGroupResource(t *testing.T) {
 }
 
 const testAccVcdDataCenterGroupNewVdc = `
+provider "vcd" {
+  alias                = "orguser"
+  user                 = "{{.OrgUser}}"
+  password             = "{{.OrgUserPassword}}"
+  auth_type            = "integrated"
+  url                  = "{{.VcdUrl}}"
+  sysorg               = "{{.Org}}"
+  org                  = "{{.Org}}"
+  vdc                  = "{{.VDC}}"
+  allow_unverified_ssl = "true"
+  max_retry_timeout    = 600
+  logging              = true
+  logging_file         = "go-vcloud-director-org.log"
+}
+
 resource "vcd_org_vdc" "newVdc" {
   name = "newVdc"
   org  = "{{.Org}}"
@@ -149,14 +298,20 @@ resource "vcd_org_vdc" "newVdc" {
   elasticity      			 = true
   include_vm_memory_overhead = true
 }
+
+data "vcd_org_vdc" "startVdc"{
+  {{if .OrgUserProvider}}{{.OrgUserProvider}}{{end}}
+
+  org  = "{{.Org}}"
+  name = "{{.VDC}}"
+}
 `
 
 const testAccVcdDataCenterGroupResource = testAccVcdDataCenterGroupNewVdc + `
-data "vcd_org_vdc" "startVdc"{
-  name = "{{.StartingVdcName}}"
-}
 
 resource "vcd_data_center_group" "fromUnitTest" {
+  {{if .OrgUserProvider}}{{.OrgUserProvider}}{{end}}
+
   org                   = "{{.Org}}"
   name                  = "{{.Name}}"
   description           = "{{.Description}}"
@@ -171,11 +326,9 @@ output "participatingVdcCount" {
 `
 
 const testAccVcdDataCenterGroupResourceUpdate = testAccVcdDataCenterGroupNewVdc + `
-data "vcd_org_vdc" "startVdc"{
-  name = "{{.StartingVdcName}}"
-}
-
 resource "vcd_data_center_group" "fromUnitTest" {
+  {{if .OrgUserProvider}}{{.OrgUserProvider}}{{end}}
+
   org                   = "{{.Org}}"
   name                  = "{{.NameUpdated}}"
   description           = "{{.DescriptionUpdate}}"
@@ -191,6 +344,8 @@ output "participatingVdcCount" {
 const testAccVcdDataCenterGroupDatasource = testAccVcdDataCenterGroupResourceUpdate + `
 # skip-binary-test: data source test only works in acceptance tests
 data "vcd_data_center_group" "fetchCreated" {
+  {{if .OrgUserProvider}}{{.OrgUserProvider}}{{end}}
+
   org  = "{{.Org}}"
   name = vcd_data_center_group.fromUnitTest.name
 }
