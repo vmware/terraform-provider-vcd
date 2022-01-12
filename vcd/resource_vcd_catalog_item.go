04cd7d61dd9c44229129c4b131b1ce7271a58db9
Giuseppe Maxia <gmaxia@vmware.com>
2022-01-12 09:08:37 +0100

    Fix precentage in progress
    Signed-off-by: Giuseppe Maxia <gmaxia@vmware.com>

--- vcd/resource_vcd_catalog_item.go
+++ vcd/resource_vcd_catalog_item.go
@@ -109,7 +109,7 @@ func resourceVcdCatalogItemCreate(d *schema.ResourceData, meta interface{}) erro
 			if err := getError(task); err != nil {
 				return err
 			}
-			logForScreen("vcd_catalog_item", fmt.Sprintf("vcd_catalog_item."+itemName+": Upload progress "+task.GetUploadProgress()+"%\n"))
+			logForScreen("vcd_catalog_item", fmt.Sprintf("vcd_catalog_item."+itemName+": Upload progress "+task.GetUploadProgress()+"%%\n"))
 			if task.GetUploadProgress() == "100.00" {
 				break
 			}
@@ -124,7 +124,7 @@ func resourceVcdCatalogItemCreate(d *schema.ResourceData, meta interface{}) erro
 				log.Printf("vCD Error importing new catalog item: %#v", err)
 				return fmt.Errorf("vCD Error importing new catalog item: %#v", err)
 			}
-			logForScreen("vcd_catalog_item", fmt.Sprintf("vcd_catalog_item."+itemName+": vCD import catalog item progress "+progress+"%\n"))
+			logForScreen("vcd_catalog_item", fmt.Sprintf("vcd_catalog_item."+itemName+": vCD import catalog item progress "+progress+"%%\n"))
 			if progress == "100" {
 				break
 			}
