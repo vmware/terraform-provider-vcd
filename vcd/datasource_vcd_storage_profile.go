295e3285e2c7887d2630287caa8a5cfcd5cf6a05
Vaidotas Bauzys <vbauzys@vmware.com>
2022-02-07 16:13:51 +0200

    added compute fields returned from API
    Signed-off-by: Vaidotas Bauzys <vbauzys@vmware.com>

--- vcd/datasource_vcd_storage_profile.go
+++ vcd/datasource_vcd_storage_profile.go
@@ -32,6 +32,70 @@ func datasourceVcdStorageProfile() *schema.Resource {
 				Required:    true,
 				Description: "Name of storage profile",
 			},
+			"limit": &schema.Schema{
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Maximum number of storage bytes (scaled by Units) allocated for this profile. A value of 0 is understood to mean `maximum possible`",
+			},
+			"used_storage": &schema.Schema{
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Storage used, in Megabytes, by the storage profile",
+			},
+			"default": &schema.Schema{
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "True if this is default storage profile for this vDC. The default storage profile is used when an object that can specify a storage profile is created with no storage profile specified",
+			},
+			"enabled": &schema.Schema{
+				Type:        schema.TypeBool,
+				Computed:    true,
+				Description: "True if this storage profile is enabled for use in the vDC",
+			},
+			"iops_allocated": &schema.Schema{
+				Type:        schema.TypeInt,
+				Computed:    true,
+				Description: "Total IOPS currently allocated to this storage profile",
+			},
+			"units": &schema.Schema{
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Scale used to define Limit",
+			},
+			"iops_settings": &schema.Schema{
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "IP ranges used for static pool allocation in the network",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"iops_limiting_enabled": &schema.Schema{
+							Type:        schema.TypeBool,
+							Computed:    true,
+							Description: "True if this storage profile is IOPs-based placement enabled",
+						},
+						"maximum_disk_iops": &schema.Schema{
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "The maximum IOPS value that this storage profile is permitted to deliver. Value of 0 means this max setting is disabled and there is no max disk IOPS restriction",
+						},
+						"default_disk_iops": &schema.Schema{
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Value of 0 for disk iops means that no iops would be reserved or provisioned for that virtual disk",
+						},
+						"disk_iops_per_gb_max": &schema.Schema{
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "The maximum disk IOPs per GB value that this storage profile is permitted to deliver. A value of 0 means there is no perGB IOPS restriction",
+						},
+						"iops_limit": &schema.Schema{
+							Type:        schema.TypeInt,
+							Computed:    true,
+							Description: "Maximum number of IOPs that can be allocated for this profile. A value of 0 is understood to mean `maximum possible`",
+						},
+					},
+				},
+			},
 		},
 	}
 }
@@ -51,5 +115,35 @@ func datasourceVcdStorageProfileRead(ctx context.Context, d *schema.ResourceData
 			govcd.ErrorEntityNotFound, name, vdc.Vdc.Name, err)
 	}
 	d.SetId(storageProfileReference.ID)
+
+	storageProfile, err := vcdClient.GetStorageProfileByHref(storageProfileReference.HREF)
+	if err != nil {
+		return diag.Errorf("%s: error fetching additional details for Storage Profile '%s' in VDC '%s': %s",
+			govcd.ErrorEntityNotFound, name, vdc.Vdc.Name, err)
+	}
+
+	dSet(d, "limit", storageProfile.Limit)
+	dSet(d, "used_storage", storageProfile.StorageUsedMB)
+	dSet(d, "default", storageProfile.Default)
+	dSet(d, "enabled", storageProfile.Enabled)
+	dSet(d, "iops_allocated", storageProfile.IopsAllocated)
+	dSet(d, "units", storageProfile.Units)
+	if storageProfile.IopsSettings != nil {
+
+		var iopsSettings = make(map[string]interface{})
+
+		iopsSettings["iops_limit"] = storageProfile.IopsSettings.StorageProfileIopsLimit
+		iopsSettings["iops_limiting_enabled"] = storageProfile.IopsSettings.Enabled
+		iopsSettings["default_disk_iops"] = storageProfile.IopsSettings.DiskIopsDefault
+		iopsSettings["maximum_disk_iops"] = storageProfile.IopsSettings.DiskIopsMax
+		iopsSettings["disk_iops_per_gb_max"] = storageProfile.IopsSettings.DiskIopsPerGbMax
+
+		iopsSettingsSlice := []map[string]interface{}{iopsSettings}
+		err = d.Set("iops_settings", iopsSettingsSlice)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
 	return nil
 }
--- website/docs/d/storage_profile.html.markdown
+++ website/docs/d/storage_profile.html.markdown
@@ -32,5 +32,23 @@ The following arguments are supported:
 * `name` - (Required) Storage profile name.
 
 ## Attribute reference
+* `id` - storage profile identifier
 
-This data source exports only `id` field.
+(Supported in provider *v3.6+*)
+
+* `limit` - Maximum number of storage bytes (scaled by Units) allocated for this profile. A value of 0 is understood to mean `maximum possible`
+* `used_storage` - Storage used, in Megabytes, by the storage profile
+* `default` - True if this is default storage profile for this vDC. The default storage profile is used when an object that can specify a storage profile is created with no storage profile specified
+* `enabled` - True if this storage profile is enabled for use in the vDC"
+* `iops_allocated` - Total IOPS currently allocated to this storage profile
+* `units` - Scale used to define Limit
+* `iops_settings` - A block providing iops settings. See [IOPS settings](#iopsSettings) below for details.
+
+<a id="iopsSettings"></a>
+## IOPS settings
+
+* `iops_limiting_enabled` - True if this storage profile is IOPs-based placement enabled
+* `maximum_disk_iops` - The maximum IOPS value that this storage profile is permitted to deliver. Value of 0 means this max setting is disabled and there is no max disk IOPS restriction
+* `default_disk_iops` - Value of 0 for disk iops means that no iops would be reserved or provisioned for that virtual disk
+* `disk_iops_per_gb_max` - The maximum disk IOPs per GB value that this storage profile is permitted to deliver. A value of 0 means there is no perGB IOPS restriction
+* `iops_limit` - Maximum number of IOPs that can be allocated for this profile. A value of 0 is understood to mean `maximum possible`
