894e49b08459a504b4b80e3399064c29b13c73b4
Dainius <Didainius@users.noreply.github.com>
2022-03-21 11:56:31 +0200

    Add VDC group support for NSX-T Edge Gateway (#793)
    This PR adds support for VDC Groups to NSX-T Edge Gateway resources and data sources
    `vcd_nsxt_edgegateway`.
    
    VDC Group support requires VCD 10.2+ and API v35.0+. 
    
    The main difference is `types.OpenAPIEdgeGateway.OrgVdc.ID` is deprecated and replaced by `types.OpenAPIEdgeGateway.OwnerRef.ID` which supports VDC Groups.
    
    Our resource has two new fields `owner_id`  (which supports VDC or VDC Group) and `starting_vdc_id`. It also deprecates `vdc` field at resource level as well as `provider` level. 
    
    **Note**. NSX-T Edge Gateway cannot be created directly in VDC Group (it must at first be created in a VDC which is a member of VDC Group).  `starting_vdc_id` allows to (optionally) specify a VDC, in which the Edge Gateway should be created while the final destination will be what is specified in `owner_id`. When `starting_vdc_id` is not specified, but `owner_id` is a VDC Group - this resource will pick random VDC in the group and then move Edge Gateway to it.
    
    The most ugly part here is backwards compatibility, migration path, and the field pick priority. Such field priority is meant to be set:
    * `owner_id` (ignores other values)
    * `vdc` at resource level
    * `vdc` inherited from `provider` section
    
    Docs are adjusted accordingly and also all examples with using `vdc` field are removed.
    
    
    Additional things:
    * Minimally patched Org Network resources using OpenAPI (full VDC support to come out in other PR to
    split cognitive load). The only change for now is VDC specification field switched from `OrgVdc` to
    `OwnerId`. The reason is that API switched to V35.0 and OrgVdc ID cannot be created anymore  (API
    returns error)
    
    
    * Changed a few MetadataFields from deprecated to new format, because staticcheck complains
    
    Ran tests with tag `nsxt`on 10.2.2.1, 10.3.1

--- vcd/datasource_vcd_nsxt_edgegateway.go
+++ vcd/datasource_vcd_nsxt_edgegateway.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/vmware/go-vcloud-director/v2/govcd"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
@@ -13,35 +15,44 @@ func datasourceVcdNsxtEdgeGateway() *schema.Resource {
 	return &schema.Resource{
 		ReadContext: datasourceVcdNsxtEdgeGatewayRead,
 		Schema: map[string]*schema.Schema{
-			"org": &schema.Schema{
+			"org": {
 				Type:     schema.TypeString,
 				Optional: true,
 				ForceNew: true,
 				Description: "The name of organization to use, optional if defined at provider " +
 					"level. Useful when connected as sysadmin working across different organizations",
 			},
-			"vdc": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				ForceNew:    true,
-				Description: "The name of VDC to use, optional if defined at provider level",
+			"vdc": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				Description:   "The name of VDC to use, optional if defined at provider level",
+				ConflictsWith: []string{"owner_id"},
+				Deprecated:    "This field is deprecated in favor of 'owner_id' which supports both - VDC and VDC group IDs",
 			},
-			"name": &schema.Schema{
+			"name": {
 				Type:        schema.TypeString,
 				Required:    true,
 				Description: "Edge Gateway name",
 			},
-			"description": &schema.Schema{
+			"owner_id": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				Description:   "ID of VDC or VDC Group",
+				ConflictsWith: []string{"vdc"},
+			},
+			"description": {
 				Type:        schema.TypeString,
 				Computed:    true,
 				Description: "Edge Gateway description",
 			},
-			"dedicate_external_network": &schema.Schema{
+			"dedicate_external_network": {
 				Type:        schema.TypeBool,
 				Computed:    true,
 				Description: "Dedicating the External Network will enable Route Advertisement for this Edge Gateway.",
 			},
-			"external_network_id": &schema.Schema{
+			"external_network_id": {
 				Type:        schema.TypeString,
 				Computed:    true,
 				Description: "External network ID",
@@ -65,7 +76,7 @@ func datasourceVcdNsxtEdgeGateway() *schema.Resource {
 						"primary_ip": {
 							Type:        schema.TypeString,
 							Computed:    true,
-							Description: "IP address on the edge gateway ",
+							Description: "IP address on the edge gateway",
 						},
 						"allocated_ips": {
 							Type:        schema.TypeSet,
@@ -92,7 +103,7 @@ func datasourceVcdNsxtEdgeGateway() *schema.Resource {
 				Computed:    true,
 				Description: "Primary IP address of edge gateway",
 			},
-			"edge_cluster_id": &schema.Schema{
+			"edge_cluster_id": {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Description: "NSX-T Edge Cluster ID.",
@@ -105,27 +116,121 @@ func datasourceVcdNsxtEdgeGatewayRead(ctx context.Context, d *schema.ResourceDat
 	log.Printf("[TRACE] NSX-T edge gateway datasource read initiated")
 
 	vcdClient := meta.(*VCDClient)
-
-	_, vdc, err := vcdClient.GetOrgAndVdcFromResource(d)
+	org, err := vcdClient.GetOrgFromResource(d)
 	if err != nil {
 		return diag.FromErr(fmt.Errorf("error retrieving Org: %s", err))
 	}
 
-	if vdc.IsNsxv() {
-		return diag.Errorf("please use 'vcd_edgegateway' for NSX-V backed VDC")
+	// Validate if VDC or VDC Group is NSX-T backed
+	inheritedVdcField := vcdClient.Vdc
+	vdcField := d.Get("vdc").(string)
+	ownerIdField := d.Get("owner_id").(string)
+
+	err = validateIfVdcOrVdcGroupIsNsxt(org, inheritedVdcField, vdcField, ownerIdField)
+	if err != nil {
+		return diag.FromErr(err)
 	}
 
-	edge, err := vdc.GetNsxtEdgeGatewayByName(d.Get("name").(string))
-	if err != nil {
-		return diag.FromErr(fmt.Errorf("could not retrieve NSX-T edge gateway: %s", err))
+	var edge *govcd.NsxtEdgeGateway
+	edgeGatewayName := d.Get("name").(string)
+	switch {
+	case ownerIdField != "":
+		edge, err = org.GetNsxtEdgeGatewayByNameAndOwnerId(edgeGatewayName, ownerIdField)
+		if err != nil {
+			return diag.Errorf("error getting NSX-T Edge Gateway:%s", err)
+		}
+	case ownerIdField == "":
+		_, vdc, err := pickVdcIdByPriority(org, inheritedVdcField, vdcField, ownerIdField)
+		if err != nil {
+			return diag.Errorf("error getting VDC ID: %s", err)
+		}
+
+		edge, err = vdc.GetNsxtEdgeGatewayByName(edgeGatewayName)
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("could not retrieve NSX-T edge gateway: %s", err))
+		}
+	default:
+		return diag.Errorf("error looking up Edge Gateway - switch did not match any cases")
 	}
 
 	err = setNsxtEdgeGatewayData(edge.EdgeGateway, d)
 	if err != nil {
-		return diag.FromErr(fmt.Errorf("error reading NSX-T edge gateway data: %s", err))
+		return diag.FromErr(fmt.Errorf("error reading NSX-T Edge Gateway data: %s", err))
 	}
 
 	d.SetId(edge.EdgeGateway.ID)
 
 	return nil
+}
+
+// validateIfVdcOrVdcGroupIsNsxt evaluates VDC field priority using pickVdcIdByPriority and then
+// checks if that VDC or VDC Group is an NSX-T one and returns an error if not
+func validateIfVdcOrVdcGroupIsNsxt(org *govcd.Org, inheritedVdcField, vdcField, ownerIdField string) error {
+	usedFieldId, _, err := pickVdcIdByPriority(org, inheritedVdcField, vdcField, ownerIdField)
+
+	if err != nil {
+		return fmt.Errorf("error finding VDC ID: %s", err)
+	}
+
+	isNsxt, err := isBackedByNsxt(org, usedFieldId)
+	if err != nil {
+		return fmt.Errorf("error ")
+	}
+
+	if !isNsxt {
+		return fmt.Errorf("please use 'vcd_edgegateway' for NSX-V backed VDC")
+	}
+
+	return nil
+}
+
+// pickVdcIdByPriority picks primary field to be used from the specified ones. The priority is such
+// * `owner_id`
+// * `vdc` at resource level
+// * `vdc` inherited from provider configuration
+func pickVdcIdByPriority(org *govcd.Org, inheritedVdcField, vdcField, ownerIdField string) (string, *govcd.Vdc, error) {
+	if ownerIdField != "" {
+		return ownerIdField, nil, nil
+	}
+
+	if vdcField != "" {
+		vdc, err := org.GetVDCByName(vdcField, false)
+		if err != nil {
+			return "", nil, fmt.Errorf("error finding VDC '%s': %s", vdc.Vdc.ID, err)
+		}
+		return vdc.Vdc.ID, vdc, nil
+	}
+
+	if inheritedVdcField != "" {
+		vdc, err := org.GetVDCByName(inheritedVdcField, false)
+		if err != nil {
+			return "", nil, fmt.Errorf("error finding VDC '%s': %s", vdc.Vdc.ID, err)
+		}
+		return vdc.Vdc.ID, vdc, nil
+	}
+
+	return "", nil, fmt.Errorf("none of the fields `owner_id`, `vdc` and provider inherited `vdc`")
+}
+
+// isBackedByNsxt accepts VDC or VDC Group ID and checks if it is backed by NSX-T
+func isBackedByNsxt(org *govcd.Org, vdcOrVdcGroupId string) (bool, error) {
+	var vdcOrGroup vdcOrVdcGroupVerifier
+	var err error
+
+	switch {
+	case govcd.OwnerIsVdc(vdcOrVdcGroupId):
+		vdcOrGroup, err = org.GetVDCById(vdcOrVdcGroupId, false)
+		if err != nil {
+			return false, err
+		}
+	case govcd.OwnerIsVdcGroup(vdcOrVdcGroupId):
+		vdcOrGroup, err = org.GetVdcGroupById(vdcOrVdcGroupId)
+		if err != nil {
+			return false, err
+		}
+	default:
+		return false, fmt.Errorf("error determining VDC type by ID '%s'", vdcOrVdcGroupId)
+	}
+
+	return vdcOrGroup.IsNsxt(), nil
 }
--- website/docs/d/nsxt_edgegateway.html.markdown
+++ website/docs/d/nsxt_edgegateway.html.markdown
@@ -10,29 +10,52 @@ description: |-
 
 Provides a VMware Cloud Director NSX-T edge gateway data source. This can be used to read NSX-T edge gateway configurations.
 
--> **Note:** This data source uses new VMware Cloud Director
-[OpenAPI](https://code.vmware.com/docs/11982/getting-started-with-vmware-cloud-director-openapi) and
-requires at least VCD *10.1.1+* and NSX-T *3.0+*.
-
 Supported in provider *v3.1+*.
 
-## Example Usage 
+## Example Usage (NSX-T Edge Gateway belonging to VDC group)
 
 ```hcl
+data "vcd_vdc_group" "group1" {
+  name = "existing-group"
+}
+
 data "vcd_nsxt_edgegateway" "t1" {
-  org  = "myorg"
-  vdc  = "my-nsxt-vdc"
-  name = "nsxt-edge-gateway"
+  org      = "myorg"
+  owner_id = data.vcd_vdc_group.group1.id
+  name     = "nsxt-edge-gateway"
 }
 ```
 
+## Example Usage (NSX-T Edge Gateway belonging to VDC)
+
+```hcl
+data "vcd_org_vdc" "vdc1" {
+  name = "existing-vdc"
+}
+
+data "vcd_nsxt_edgegateway" "t1" {
+  org      = "myorg"
+  owner_id = data.vcd_org_vdc.vdc1.id
+  name     = "nsxt-edge-gateway"
+}
+```
 
 ## Argument Reference
 
 The following arguments are supported:
 
-* `org` - (Optional) The name of organization to which the edge gatweway belongs. Optional if defined at provider level.
-* `vdc` - (Optional) The name of VDC that owns the edge gateway. Optional if defined at provider level.
+* `org` - (Optional) The name of organization to which the NSX-T Edge Gateway belongs. Optional if
+  defined at provider level.
+* `vdc` - (Optional)  **Deprecated** - please use `owner_id` field. The name of VDC that owns the
+  NSX-T Edge Gateway. Optional if defined at provider level.
+* `owner_id` - (Optional, *v3.6+*,*VCD 10.2+*) **Replaces** `vdc` field. The ID of VDC or VDC group
+that this Edge Gateway belongs to. **Note:** Data source
+[vcd_vdc_group](/providers/vmware/vcd/latest/docs/data-sources/vdc_group) can be used to lookup ID
+by name.
+
+~> Only one of `vdc` or `owner_id` can be specified. `owner_id` takes precedence over `vdc`
+definition at provider level.
+
 * `name` - (Required) NSX-T Edge Gateway name.
 
 ## Attribute reference
