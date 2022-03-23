e726baf9e9a584855f052bdc50e96c870c72f0b7
abarreiro <abarreiro@vmware.com>
2022-03-23 09:56:00 +0100

    Fix typo in datasource
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- vcd/resource_vcd_org_group_test.go
+++ vcd/resource_vcd_org_group_test.go
@@ -186,7 +186,7 @@ func TestAccVcdOrgGroup(t *testing.T) {
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resourceFieldsEqual("data.vcd_org_group.sourced_group1", "vcd_org_group.group1", nil),
 					resourceFieldsEqual("data.vcd_org_group.sourced_group2", "vcd_org_group.group2", nil),
-					resourceFieldsEqual("data.vcd_org_usergb.sourced_user1", "vcd_org_user.user1", nil),
+					resourceFieldsEqual("data.vcd_org_user.sourced_user1", "vcd_org_user.user1", nil),
 				),
 			},
 			{
