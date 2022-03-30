5832dbf9b4f812ef7c35706a7e4f46b0da20e18f
Vaidotas Bauzys <vbauzys@vmware.com>
2022-03-30 11:53:05 +0300

    small fixes
    Signed-off-by: Vaidotas Bauzys <vbauzys@vmware.com>

--- vcd/datasource_vcd_nsxt_ip_set.go
+++ vcd/datasource_vcd_nsxt_ip_set.go
@@ -74,43 +74,42 @@ func datasourceVcdNsxtIpSetRead(ctx context.Context, d *schema.ResourceData, met
 	var ipSet *govcd.NsxtFirewallGroup
 	var parentVdcOrVdcGroupId string
 
-	if ipSetName != "" && edgeGatewayId != "" {
-		// Lookup Edge Gateway to know parent VDC or VDC Group
-		anyEdgeGateway, err := org.GetAnyTypeEdgeGatewayById(edgeGatewayId)
+	if ipSetName == "" || edgeGatewayId == "" {
+		return diag.Errorf("error - not all parameters specified for NSX-T IP Set lookup")
+	}
+	// Lookup Edge Gateway to know parent VDC or VDC Group
+	anyEdgeGateway, err := org.GetAnyTypeEdgeGatewayById(edgeGatewayId)
+	if err != nil {
+		return diag.Errorf("[nsxt ip set read] error retrieving Edge Gateway structure: %s", err)
+	}
+	if anyEdgeGateway.IsNsxv() {
+		return diag.Errorf("[nsxt ip set read] NSX-V edge gateway not supported")
+	}
+
+	parentVdcOrVdcGroupId = anyEdgeGateway.EdgeGateway.OwnerRef.ID
+
+	if govcd.OwnerIsVdcGroup(parentVdcOrVdcGroupId) {
+		vdcGroup, err := org.GetVdcGroupById(parentVdcOrVdcGroupId)
 		if err != nil {
-			return diag.Errorf("[nsxt ip set read] error retrieving Edge Gateway structure: %s", err)
-		}
-		if anyEdgeGateway.IsNsxv() {
-			return diag.Errorf("[nsxt ip set read] NSX-V edge gateway not supported")
+			return diag.Errorf("could not retrieve VDC Group with ID '%s': %s", d.Id(), err)
 		}
 
-		parentVdcOrVdcGroupId = anyEdgeGateway.EdgeGateway.OwnerRef.ID
-
-		if govcd.OwnerIsVdcGroup(parentVdcOrVdcGroupId) {
-			vdcGroup, err := org.GetVdcGroupById(parentVdcOrVdcGroupId)
-			if err != nil {
-				return diag.Errorf("could not retrieve VDC Group with ID '%s': %s", d.Id(), err)
-			}
-
-			// Name uniqueness is enforced by VCD for types.FirewallGroupTypeIpSet
-			ipSet, err = vdcGroup.GetNsxtFirewallGroupByName(d.Get("name").(string), types.FirewallGroupTypeIpSet)
-			if err != nil {
-				return diag.Errorf("[nsxt ip set read] error getting NSX-T IP Set with Name '%s': %s", d.Get("name").(string), err)
-			}
-		} else {
-			nsxtEdgeGateway, err := anyEdgeGateway.GetNsxtEdgeGateway()
-			if err != nil {
-				return diag.Errorf("could not retrieve NSX-T Edge Gateway with ID '%s': %s", d.Id(), err)
-			}
-
-			// Name uniqueness is enforced by VCD for types.FirewallGroupTypeIpSet
-			ipSet, err = nsxtEdgeGateway.GetNsxtFirewallGroupByName(d.Get("name").(string), types.FirewallGroupTypeIpSet)
-			if err != nil {
-				return diag.Errorf("[nsxt ip set read] error getting NSX-T IP Set with Name '%s': %s", d.Get("name").(string), err)
-			}
+		// Name uniqueness is enforced by VCD for types.FirewallGroupTypeIpSet
+		ipSet, err = vdcGroup.GetNsxtFirewallGroupByName(d.Get("name").(string), types.FirewallGroupTypeIpSet)
+		if err != nil {
+			return diag.Errorf("[nsxt ip set read] error getting NSX-T IP Set with Name '%s': %s", d.Get("name").(string), err)
 		}
 	} else {
-		return diag.Errorf("error - not all parameters specified for NSX-T IP Set lookup")
+		nsxtEdgeGateway, err := anyEdgeGateway.GetNsxtEdgeGateway()
+		if err != nil {
+			return diag.Errorf("could not retrieve NSX-T Edge Gateway with ID '%s': %s", d.Id(), err)
+		}
+
+		// Name uniqueness is enforced by VCD for types.FirewallGroupTypeIpSet
+		ipSet, err = nsxtEdgeGateway.GetNsxtFirewallGroupByName(d.Get("name").(string), types.FirewallGroupTypeIpSet)
+		if err != nil {
+			return diag.Errorf("[nsxt ip set read] error getting NSX-T IP Set with Name '%s': %s", d.Get("name").(string), err)
+		}
 	}
 
 	err = setNsxtIpSetData(d, ipSet.NsxtFirewallGroup, parentVdcOrVdcGroupId)
