b2ae85b483e68e43e75f23092e4851e5f3f50799
Vaidotas Bauzys <vbauzys@vmware.com>
2022-04-04 08:49:49 +0300

    improvement
    Signed-off-by: Vaidotas Bauzys <vbauzys@vmware.com>

--- vcd/nsxt_common.go
+++ vcd/nsxt_common.go
@@ -7,24 +7,24 @@ import (
 )
 
 // getParentEdgeGatewayOwnerIdAndNsxtEdgeGateway returns VDC or VDC group ID and NSX-T Edge Gateway type
-func getParentEdgeGatewayOwnerIdAndNsxtEdgeGateway(vcdClient *VCDClient, d *schema.ResourceData, action string) (string, *govcd.NsxtEdgeGateway, error) {
+func getParentEdgeGatewayOwnerIdAndNsxtEdgeGateway(vcdClient *VCDClient, d *schema.ResourceData, actionMessage string) (string, *govcd.NsxtEdgeGateway, error) {
 	org, err := vcdClient.GetOrgFromResource(d)
 	if err != nil {
-		return "", nil, fmt.Errorf("[%s] error retrieving Org: %s", action, err)
+		return "", nil, fmt.Errorf("[%s] error retrieving Org: %s", actionMessage, err)
 	}
 
 	// Lookup Edge Gateway to know parent VDC or VDC Group
 	anyEdgeGateway, err := org.GetAnyTypeEdgeGatewayById(d.Get("edge_gateway_id").(string))
 	if err != nil {
-		return "", nil, fmt.Errorf("[%s] error retrieving Edge Gateway structure: %s", action, err)
+		return "", nil, fmt.Errorf("[%s] error retrieving Edge Gateway structure: %s", actionMessage, err)
 	}
 	if anyEdgeGateway.IsNsxv() {
-		return "", nil, fmt.Errorf("[nsxt ip set %s] NSX-V edge gateway not supported", action)
+		return "", nil, fmt.Errorf("[%s] NSX-V edge gateway not supported", actionMessage)
 	}
 
 	nsxtEdgeGateway, err := anyEdgeGateway.GetNsxtEdgeGateway()
 	if err != nil {
-		return "", nil, fmt.Errorf("[%s] could not retrieve NSX-T Edge Gateway with ID '%s': %s", action, d.Id(), err)
+		return "", nil, fmt.Errorf("[%s] could not retrieve NSX-T Edge Gateway with ID '%s': %s", actionMessage, d.Id(), err)
 	}
 
 	return anyEdgeGateway.EdgeGateway.OwnerRef.ID, nsxtEdgeGateway, nil
