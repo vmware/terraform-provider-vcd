d830d27ec262c4275e2f6df0eb7b6492720d7d14
Miguel Sama <msama@vmware.com>
2022-05-18 16:32:13 +0200

    Add thorough testing
    Signed-off-by: Miguel Sama <msama@vmware.com>

--- vcd/resource_vcd_org_vdc_access_control_test.go
+++ vcd/resource_vcd_org_vdc_access_control_test.go
@@ -22,15 +22,23 @@ func TestAccVcdOrgVdcAccessControl(t *testing.T) {
 	userName1 := strings.ToLower(t.Name())
 	userName2 := strings.ToLower(t.Name()) + "2"
 	accessControlName := "test-access-control"
+	newVdcName := t.Name()
 
 	var params = StringMap{
-		"Org":               testConfig.VCD.Org,
-		"Vdc":               testConfig.VCD.Vdc,
-		"AccessControlName": accessControlName,
-		"UserName":          userName1,
-		"UserName2":         userName2,
-		"PasswordFile":      orgUserPasswordFile,
-		"RoleName":          govcd.OrgUserRoleOrganizationAdministrator,
+		"Org":                       testConfig.VCD.Org,
+		"Vdc":                       testConfig.VCD.Vdc,
+		"AccessControlName":         accessControlName,
+		"AccessControlName2":        accessControlName + "2",
+		"UserName":                  userName1,
+		"UserName2":                 userName2,
+		"PasswordFile":              orgUserPasswordFile,
+		"RoleName":                  govcd.OrgUserRoleOrganizationAdministrator,
+		"NewVdcName":                newVdcName,
+		"NetworkPool":               testConfig.VCD.ProviderVdc.NetworkPool,
+		"ProviderVdc":               testConfig.VCD.ProviderVdc.Name,
+		"Allocated":                 "1024",
+		"Limit":                     "1024",
+		"ProviderVdcStorageProfile": testConfig.VCD.ProviderVdc.StorageProfile,
 	}
 
 	params["FuncName"] = t.Name() + "step1"
@@ -39,6 +47,14 @@ func TestAccVcdOrgVdcAccessControl(t *testing.T) {
 
 	params["FuncName"] = t.Name() + "step2"
 	configTex2 := templateFill(testAccCheckVcdAccessControlStep2, params)
+	debugPrintf("#[DEBUG] CONFIGURATION: %s", configText)
+
+	params["FuncName"] = t.Name() + "step3"
+	configText3 := templateFill(testAccCheckVcdAccessControlStep3, params)
+	debugPrintf("#[DEBUG] CONFIGURATION: %s", configText)
+
+	params["FuncName"] = t.Name() + "step4"
+	configText4 := templateFill(testAccCheckVcdAccessControlStep4, params)
 	debugPrintf("#[DEBUG] CONFIGURATION: %s", configText)
 
 	if vcdShortTest {
@@ -54,14 +70,14 @@ func TestAccVcdOrgVdcAccessControl(t *testing.T) {
 			{
 				Config: configText,
 				Check: resource.ComposeTestCheckFunc(
-					assertVdcAccessControlIsSharedWithEverybody(),
+					assertVdcAccessControlIsSharedWithEverybody(testConfig.VCD.Vdc),
 				),
 			},
 			{
 				Config: configTex2,
 				Check: resource.ComposeTestCheckFunc(
-					assertVdcAccessControlIsSharedWithSpecificUser(userName1),
-					assertVdcAccessControlIsSharedWithSpecificUser(userName2),
+					assertVdcAccessControlIsSharedWithSpecificUser(userName1, testConfig.VCD.Vdc),
+					assertVdcAccessControlIsSharedWithSpecificUser(userName2, testConfig.VCD.Vdc),
 				),
 			},
 			{
@@ -69,6 +85,19 @@ func TestAccVcdOrgVdcAccessControl(t *testing.T) {
 				ImportState:       true,
 				ImportStateVerify: true,
 				ImportStateId:     testConfig.VCD.Org + "." + testConfig.VCD.Vdc,
+			},
+			{
+				Config: configText3,
+				Check: resource.ComposeTestCheckFunc(
+					assertVdcAccessControlIsSharedWithEverybody(newVdcName),
+				),
+			},
+			{
+				Config: configText4,
+				Check: resource.ComposeTestCheckFunc(
+					assertVdcAccessControlIsSharedWithSpecificUser(userName1, newVdcName),
+					assertVdcAccessControlIsSharedWithSpecificUser(userName2, newVdcName),
+				),
 			},
 		},
 	})
@@ -91,6 +120,117 @@ resource "vcd_org_user" "{{.UserName}}" {
   role           = "{{.RoleName}}"
   take_ownership = true
 }
+resource "vcd_org_user" "{{.UserName2}}" {
+  org            = "{{.Org}}"
+  name           = "{{.UserName2}}"
+  password_file  = "{{.PasswordFile}}"
+  role           = "{{.RoleName}}"
+  take_ownership = true
+}
+resource "vcd_org_vdc_access_control" "{{.AccessControlName}}" {
+  org                   = "{{.Org}}"
+  vdc                   = "{{.Vdc}}"
+  shared_with_everyone  = false
+  shared_with {
+    user_id             = vcd_org_user.{{.UserName}}.id
+    access_level        = "ReadOnly"
+  }
+  shared_with {
+    user_id             = vcd_org_user.{{.UserName2}}.id
+    access_level        = "ReadOnly"
+  }
+}
+`
+
+const testAccCheckVcdAccessControlStep3 = `
+resource "vcd_org_vdc" "{{.NewVdcName}}" {
+  name = "{{.NewVdcName}}"
+  org  = "{{.Org}}"
+
+  allocation_model  = "Flex"
+  network_pool_name = "{{.NetworkPool}}"
+  provider_vdc_name = "{{.ProviderVdc}}"
+
+  compute_capacity {
+    cpu {
+      allocated = "{{.Allocated}}"
+      limit     = "{{.Limit}}"
+    }
+
+    memory {
+      allocated = "{{.Allocated}}"
+      limit     = "{{.Limit}}"
+    }
+  }
+
+  storage_profile {
+    name    = "{{.ProviderVdcStorageProfile}}"
+    enabled = true
+    limit   = 10240
+    default = true
+  }
+
+  enabled                    = true
+  enable_thin_provisioning   = true
+  enable_fast_provisioning   = true
+  delete_force               = true
+  delete_recursive           = true
+  elasticity                 = false
+  include_vm_memory_overhead = false
+}
+
+resource "vcd_org_vdc_access_control" "{{.AccessControlName2}}" {
+  org                   = "{{.Org}}"
+  vdc_id                = vcd_org_vdc.{{.NewVdcName}}.id
+  shared_with_everyone  = true
+  everyone_access_level = "ReadOnly"
+}
+`
+
+const testAccCheckVcdAccessControlStep4 = `
+resource "vcd_org_vdc" "{{.NewVdcName}}" {
+  name = "{{.NewVdcName}}"
+  org  = "{{.Org}}"
+
+  allocation_model  = "Flex"
+  network_pool_name = "{{.NetworkPool}}"
+  provider_vdc_name = "{{.ProviderVdc}}"
+
+  compute_capacity {
+    cpu {
+      allocated = "{{.Allocated}}"
+      limit     = "{{.Limit}}"
+    }
+
+    memory {
+      allocated = "{{.Allocated}}"
+      limit     = "{{.Limit}}"
+    }
+  }
+
+  storage_profile {
+    name    = "{{.ProviderVdcStorageProfile}}"
+    enabled = true
+    limit   = 10240
+    default = true
+  }
+
+  enabled                    = true
+  enable_thin_provisioning   = true
+  enable_fast_provisioning   = true
+  delete_force               = true
+  delete_recursive           = true
+  elasticity                 = false
+  include_vm_memory_overhead = false
+}
+
+resource "vcd_org_user" "{{.UserName}}" {
+  org            = "{{.Org}}"
+  name           = "{{.UserName}}"
+  password_file  = "{{.PasswordFile}}"
+  role           = "{{.RoleName}}"
+  take_ownership = true
+}
 
 resource "vcd_org_user" "{{.UserName2}}" {
   org            = "{{.Org}}"
@@ -100,16 +240,14 @@ resource "vcd_org_user" "{{.UserName2}}" {
   take_ownership = true
 }
 
-resource "vcd_org_vdc_access_control" "{{.AccessControlName}}" {
+resource "vcd_org_vdc_access_control" "{{.AccessControlName2}}" {
   org                   = "{{.Org}}"
-  vdc                   = "{{.Vdc}}"
+  vdc_id                = vcd_org_vdc.{{.NewVdcName}}.id
   shared_with_everyone  = false
-
   shared_with {
     user_id             = vcd_org_user.{{.UserName}}.id
     access_level        = "ReadOnly"
   }
-
   shared_with {
     user_id             = vcd_org_user.{{.UserName2}}.id
     access_level        = "ReadOnly"
@@ -138,11 +276,11 @@ func testAccCheckVDCControlAccessDestroy() resource.TestCheckFunc {
 	}
 }
 
-func assertVdcAccessControlIsSharedWithEverybody() resource.TestCheckFunc {
+func assertVdcAccessControlIsSharedWithEverybody(vdcName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		conn := testAccProvider.Meta().(*VCDClient)
 
-		_, vdc, err := conn.GetOrgAndVdc(testConfig.VCD.Org, testConfig.VCD.Vdc)
+		_, vdc, err := conn.GetOrgAndVdc(testConfig.VCD.Org, vdcName)
 		if err != nil {
 			return fmt.Errorf("error retrieving Org %s - %s", testConfig.VCD.Org, err)
 		}
@@ -160,11 +298,11 @@ func assertVdcAccessControlIsSharedWithEverybody() resource.TestCheckFunc {
 	}
 }
 
-func assertVdcAccessControlIsSharedWithSpecificUser(userName string) resource.TestCheckFunc {
+func assertVdcAccessControlIsSharedWithSpecificUser(userName string, vdcName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		conn := testAccProvider.Meta().(*VCDClient)
 
-		_, vdc, err := conn.GetOrgAndVdc(testConfig.VCD.Org, testConfig.VCD.Vdc)
+		_, vdc, err := conn.GetOrgAndVdc(testConfig.VCD.Org, vdcName)
 		if err != nil {
 			return fmt.Errorf("error retrieving Org %s - %s", testConfig.VCD.Org, err)
 		}
