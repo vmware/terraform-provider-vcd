ee34defdc97045d56b791850a58da2bcd7da1a97
Miguel Sama <msama@vmware.com>
2022-05-26 15:42:46 +0200

    Improve testing
    Signed-off-by: Miguel Sama <msama@vmware.com>

--- vcd/resource_vcd_nsxt_route_advertisement_test.go
+++ vcd/resource_vcd_nsxt_route_advertisement_test.go
@@ -1,8 +1,11 @@
 package vcd
 
 import (
+	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"regexp"
+	"strconv"
 	"testing"
 )
 
@@ -10,18 +13,18 @@ func TestAccVcdNsxtRouteAdvertisement(t *testing.T) {
 	preTestChecks(t)
 	skipNoNsxtConfiguration(t)
 
+	isRouteAdvertisementEnable := true
 	subnet1 := "192.168.1.0/24"
-	subnet2 := "172.16.0.0/24"
+	subnet2 := "192.168.2.0/24"
 
 	// String map to fill the template
 	var params = StringMap{
-		"Org":                            testConfig.VCD.Org,
-		"NsxtVdc":                        testConfig.Nsxt.Vdc,
-		"EdgeGw":                         testConfig.Nsxt.EdgeGateway,
-		"RouteAdvertisementResourceName": t.Name(),
-		"Enabled":                        "true",
-		"Subnet1Cidr":                    subnet1,
-		"Subnet2Cidr":                    subnet2,
+		"Org":         testConfig.VCD.Org,
+		"NsxtVdc":     testConfig.Nsxt.Vdc,
+		"EdgeGw":      testConfig.Nsxt.EdgeGateway,
+		"Enabled":     strconv.FormatBool(isRouteAdvertisementEnable),
+		"Subnet1Cidr": subnet1,
+		"Subnet2Cidr": subnet2,
 	}
 
 	configText1 := templateFill(testAccNsxtRouteAdvertisementCreation, params)
@@ -43,11 +46,22 @@ func TestAccVcdNsxtRouteAdvertisement(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: configText1,
-				Check:  resource.ComposeAggregateTestCheckFunc(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestMatchResourceAttr("vcd_nsxt_route_advertisement.testing", "id", regexp.MustCompile(`^urn:vcloud:gateway:.*$`)),
+					resource.TestCheckResourceAttr("vcd_nsxt_route_advertisement.testing", "enabled", strconv.FormatBool(isRouteAdvertisementEnable)),
+					resource.TestCheckResourceAttr("vcd_nsxt_route_advertisement.testing", "subnets.#", "1"),
+					resource.TestCheckResourceAttr("vcd_nsxt_route_advertisement.testing", "subnets.0", subnet1),
+				),
 			},
 			{
 				Config: configText2,
-				Check:  resource.ComposeAggregateTestCheckFunc(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestMatchResourceAttr("vcd_nsxt_route_advertisement.testing", "id", regexp.MustCompile(`^urn:vcloud:gateway:.*$`)),
+					resource.TestCheckResourceAttr("vcd_nsxt_route_advertisement.testing", "enabled", strconv.FormatBool(isRouteAdvertisementEnable)),
+					resource.TestCheckResourceAttr("vcd_nsxt_route_advertisement.testing", "subnets.#", "2"),
+					resource.TestMatchResourceAttr("vcd_nsxt_route_advertisement.testing", "subnets.0", regexp.MustCompile(`^192.168.[1-2].0$`)),
+					resource.TestMatchResourceAttr("vcd_nsxt_route_advertisement.testing", "subnets.1", regexp.MustCompile(`^192.168.[1-2].0$`)),
+				),
 			},
 			// Import!!!!
 		},
@@ -65,7 +79,7 @@ data "vcd_nsxt_edgegateway" "{{.EdgeGw}}" {
   name     = "{{.EdgeGw}}"
 }
 
-resource "vcd_nsxt_route_advertisement" "{{.RouteAdvertisementResourceName}}" {
+resource "vcd_nsxt_route_advertisement" "testing" {
   edge_gateway_id = data.vcd_nsxt_edgegateway.{{.EdgeGw}}.id
   enabled = {{.Enabled}}
   subnets = [{{.Subnet1Cidr}}]
@@ -83,7 +97,7 @@ data "vcd_nsxt_edgegateway" "{{.EdgeGw}}" {
   name     = "{{.EdgeGw}}"
 }
 
-resource "vcd_nsxt_route_advertisement" "{{.RouteAdvertisementResourceName}}" {
+resource "vcd_nsxt_route_advertisement" "testing" {
   edge_gateway_id = data.vcd_nsxt_edgegateway.{{.EdgeGw}}.id
   enabled = {{.Enabled}}
   subnets = [{{.Subnet1Cidr}}, {{.Subnet2Cidr}}]
@@ -92,7 +106,31 @@ resource "vcd_nsxt_route_advertisement" "{{.RouteAdvertisementResourceName}}" {
 
 func testAccCheckNsxtRouteAdvertisement(vdcName, edgeGatewayName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		// TBD
+		conn := testAccProvider.Meta().(*VCDClient)
+
+		_, vdc, err := conn.GetOrgAndVdc(testConfig.VCD.Org, vdcName)
+		if err != nil {
+			return fmt.Errorf(errorRetrievingVdcFromOrg, vdcName, testConfig.VCD.Org, err)
+		}
+
+		edge, err := vdc.GetNsxtEdgeGatewayByName(edgeGatewayName)
+		if err != nil {
+			return fmt.Errorf(errorUnableToFindEdgeGateway, edgeGatewayName)
+		}
+
+		routeAdvertisement, err := edge.GetNsxtRouteAdvertisement(true)
+		if err != nil {
+			return fmt.Errorf("error trying to retrieve route advertisement - %s", err)
+		}
+
+		if routeAdvertisement.Enable {
+			return fmt.Errorf("error destroying route advertisement. Wanted routeAdvertisement.Enable false, Got %t", routeAdvertisement.Enable)
+		}
+
+		if routeAdvertisement.Subnets != nil && len(routeAdvertisement.Subnets) > 0 {
+			return fmt.Errorf("error destroying route advertisement. Wanted 0 routeAdvertisement.Subnets, got %d", len(routeAdvertisement.Subnets))
+		}
+
 		return nil
 	}
 }
