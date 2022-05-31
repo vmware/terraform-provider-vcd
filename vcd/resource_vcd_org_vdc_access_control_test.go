557a0b21985021a039a3462ae5f88c2a77e1d039
Miguel Sama <msama@vmware.com>
2022-05-31 11:29:20 +0200

    Remove vdc_id tests and migrate to nsxt vdc
    Signed-off-by: Miguel Sama <msama@vmware.com>

--- vcd/resource_vcd_org_vdc_access_control_test.go
+++ vcd/resource_vcd_org_vdc_access_control_test.go
@@ -22,23 +22,16 @@ func TestAccVcdOrgVdcAccessControl(t *testing.T) {
 	userName1 := strings.ToLower(t.Name())
 	userName2 := strings.ToLower(t.Name()) + "2"
 	accessControlName := "test-access-control"
-	newVdcName := t.Name()
 
 	var params = StringMap{
-		"Org":                       testConfig.VCD.Org,
-		"Vdc":                       testConfig.VCD.Vdc,
-		"AccessControlName":         accessControlName,
-		"AccessControlName2":        accessControlName + "2",
-		"UserName":                  userName1,
-		"UserName2":                 userName2,
-		"PasswordFile":              orgUserPasswordFile,
-		"RoleName":                  govcd.OrgUserRoleOrganizationAdministrator,
-		"NewVdcName":                newVdcName,
-		"NetworkPool":               testConfig.VCD.ProviderVdc.NetworkPool,
-		"ProviderVdc":               testConfig.VCD.ProviderVdc.Name,
-		"Allocated":                 "1024",
-		"Limit":                     "1024",
-		"ProviderVdcStorageProfile": testConfig.VCD.ProviderVdc.StorageProfile,
+		"Org":                testConfig.VCD.Org,
+		"Vdc":                testConfig.Nsxt.Vdc,
+		"AccessControlName":  accessControlName,
+		"AccessControlName2": accessControlName + "2",
+		"UserName":           userName1,
+		"UserName2":          userName2,
+		"PasswordFile":       orgUserPasswordFile,
+		"RoleName":           govcd.OrgUserRoleOrganizationAdministrator,
 	}
 
 	params["FuncName"] = t.Name() + "step1"
@@ -47,14 +40,6 @@ func TestAccVcdOrgVdcAccessControl(t *testing.T) {
 
 	params["FuncName"] = t.Name() + "step2"
 	configTex2 := templateFill(testAccCheckVcdAccessControlStep2, params)
-	debugPrintf("#[DEBUG] CONFIGURATION: %s", configText)
-
-	params["FuncName"] = t.Name() + "step3"
-	configText3 := templateFill(testAccCheckVcdAccessControlStep3, params)
-	debugPrintf("#[DEBUG] CONFIGURATION: %s", configText)
-
-	params["FuncName"] = t.Name() + "step4"
-	configText4 := templateFill(testAccCheckVcdAccessControlStep4, params)
 	debugPrintf("#[DEBUG] CONFIGURATION: %s", configText)
 
 	if vcdShortTest {
@@ -70,34 +55,21 @@ func TestAccVcdOrgVdcAccessControl(t *testing.T) {
 			{
 				Config: configText,
 				Check: resource.ComposeTestCheckFunc(
-					assertVdcAccessControlIsSharedWithEverybody(testConfig.VCD.Vdc),
+					assertVdcAccessControlIsSharedWithEverybody(testConfig.Nsxt.Vdc),
 				),
 			},
 			{
 				Config: configTex2,
 				Check: resource.ComposeTestCheckFunc(
-					assertVdcAccessControlIsSharedWithSpecificUser(userName1, testConfig.VCD.Vdc),
-					assertVdcAccessControlIsSharedWithSpecificUser(userName2, testConfig.VCD.Vdc),
+					assertVdcAccessControlIsSharedWithSpecificUser(userName1, testConfig.Nsxt.Vdc),
+					assertVdcAccessControlIsSharedWithSpecificUser(userName2, testConfig.Nsxt.Vdc),
 				),
 			},
 			{
 				ResourceName:      fmt.Sprintf("vcd_org_vdc_access_control.%s", accessControlName),
 				ImportState:       true,
 				ImportStateVerify: true,
-				ImportStateId:     testConfig.VCD.Org + "." + testConfig.VCD.Vdc,
-			},
-			{
-				Config: configText3,
-				Check: resource.ComposeTestCheckFunc(
-					assertVdcAccessControlIsSharedWithEverybody(newVdcName),
-				),
-			},
-			{
-				Config: configText4,
-				Check: resource.ComposeTestCheckFunc(
-					assertVdcAccessControlIsSharedWithSpecificUser(userName1, newVdcName),
-					assertVdcAccessControlIsSharedWithSpecificUser(userName2, newVdcName),
-				),
+				ImportStateId:     testConfig.VCD.Org + "." + testConfig.Nsxt.Vdc,
 			},
 		},
 	})
@@ -139,123 +111,6 @@ resource "vcd_org_vdc_access_control" "{{.AccessControlName}}" {
     user_id             = vcd_org_user.{{.UserName2}}.id
     access_level        = "ReadOnly"
   }
-}
-`
-
-const testAccCheckVcdAccessControlStep3 = `
-resource "vcd_org_vdc" "{{.NewVdcName}}" {
-  name = "{{.NewVdcName}}"
-  org  = "{{.Org}}"
-
-  allocation_model  = "Flex"
-  network_pool_name = "{{.NetworkPool}}"
-  provider_vdc_name = "{{.ProviderVdc}}"
-
-  compute_capacity {
-    cpu {
-      allocated = "{{.Allocated}}"
-      limit     = "{{.Limit}}"
-    }
-
-    memory {
-      allocated = "{{.Allocated}}"
-      limit     = "{{.Limit}}"
-    }
-  }
-
-  storage_profile {
-    name    = "{{.ProviderVdcStorageProfile}}"
-    enabled = true
-    limit   = 10240
-    default = true
-  }
-
-  enabled                    = true
-  enable_thin_provisioning   = true
-  enable_fast_provisioning   = true
-  delete_force               = true
-  delete_recursive           = true
-  elasticity                 = false
-  include_vm_memory_overhead = false
-}
-
-resource "vcd_org_vdc_access_control" "{{.AccessControlName2}}" {
-  org                   = "{{.Org}}"
-  vdc                   = vcd_org_vdc.{{.NewVdcName}}.name
-  shared_with_everyone  = true
-  everyone_access_level = "ReadOnly"
-
-  depends_on            = [ vcd_org_vdc.{{.NewVdcName}} ]
-}
-`
-
-const testAccCheckVcdAccessControlStep4 = `
-resource "vcd_org_vdc" "{{.NewVdcName}}" {
-  name = "{{.NewVdcName}}"
-  org  = "{{.Org}}"
-
-  allocation_model  = "Flex"
-  network_pool_name = "{{.NetworkPool}}"
-  provider_vdc_name = "{{.ProviderVdc}}"
-
-  compute_capacity {
-    cpu {
-      allocated = "{{.Allocated}}"
-      limit     = "{{.Limit}}"
-    }
-
-    memory {
-      allocated = "{{.Allocated}}"
-      limit     = "{{.Limit}}"
-    }
-  }
-
-  storage_profile {
-    name    = "{{.ProviderVdcStorageProfile}}"
-    enabled = true
-    limit   = 10240
-    default = true
-  }
-
-  enabled                    = true
-  enable_thin_provisioning   = true
-  enable_fast_provisioning   = true
-  delete_force               = true
-  delete_recursive           = true
-  elasticity                 = false
-  include_vm_memory_overhead = false
-}
-
-resource "vcd_org_user" "{{.UserName}}" {
-  org            = "{{.Org}}"
-  name           = "{{.UserName}}"
-  password_file  = "{{.PasswordFile}}"
-  role           = "{{.RoleName}}"
-  take_ownership = true
-}
-
-resource "vcd_org_user" "{{.UserName2}}" {
-  org            = "{{.Org}}"
-  name           = "{{.UserName2}}"
-  password_file  = "{{.PasswordFile}}"
-  role           = "{{.RoleName}}"
-  take_ownership = true
-}
-
-resource "vcd_org_vdc_access_control" "{{.AccessControlName2}}" {
-  org                   = "{{.Org}}"
-  vdc                   = vcd_org_vdc.{{.NewVdcName}}.name
-  shared_with_everyone  = false
-  shared_with {
-    user_id             = vcd_org_user.{{.UserName}}.id
-    access_level        = "ReadOnly"
-  }
-  shared_with {
-    user_id             = vcd_org_user.{{.UserName2}}.id
-    access_level        = "ReadOnly"
-  }
-  
-  depends_on            = [ vcd_org_vdc.{{.NewVdcName}} ]
 }
 `
 
