962aa5675a6af64113d5a8b7808c87c752a9aa84
Adam Barreiro <adambarreiro@users.noreply.github.com>
2022-06-03 10:36:30 +0200

    Refactor metadata handling (#853)
    * Change the loop that iterates over all metadata to add entries one by one in favor of the MergeMetadata operation, that adds everything at once.
    * Remove duplicated code and centralises metadata handling in a single function.
    
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/vmware/terraform-provider-vcd/v3
 go 1.13
 
 require (
-	github.com/hashicorp/go-version v1.4.0
-	github.com/hashicorp/terraform-plugin-sdk/v2 v2.14.0
+	github.com/hashicorp/go-version v1.5.0
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.17.0
 	github.com/kr/pretty v0.2.1
-	github.com/vmware/go-vcloud-director/v2 v2.16.0-alpha.6
+	github.com/vmware/go-vcloud-director/v2 v2.16.0-alpha.7
 )
--- vcd/structure.go
+++ vcd/structure.go
@@ -155,3 +155,41 @@ func convertSliceOfStringsToOpenApiReferenceIds(ids []string) []types.OpenApiRef
 
 	return resultReferences
 }
+
+// MetadataCompatible allows to consider all structs that implement metadata handling to be the same type
+type metadataCompatible interface {
+	GetMetadata() (*types.Metadata, error)
+	AddMetadataEntry(typedValue, key, value string) error
+	MergeMetadata(typedValue string, metadata map[string]interface{}) error
+	DeleteMetadataEntry(key string) error
+}
+
+// createOrUpdateOrgMetadata creates or updates metadata entries for the given resource and attribute name
+func createOrUpdateMetadata(d *schema.ResourceData, resource metadataCompatible, attributeName string) error {
+	if d.HasChange(attributeName) {
+		oldRaw, newRaw := d.GetChange(attributeName)
+		oldMetadata := oldRaw.(map[string]interface{})
+		newMetadata := newRaw.(map[string]interface{})
+		var toBeRemovedMetadata []string
+		// Check if any key in old metadata was removed in new metadata.
+		// Creates a list of keys to be removed.
+		for k := range oldMetadata {
+			if _, ok := newMetadata[k]; !ok {
+				toBeRemovedMetadata = append(toBeRemovedMetadata, k)
+			}
+		}
+		for _, k := range toBeRemovedMetadata {
+			err := resource.DeleteMetadataEntry(k)
+			if err != nil {
+				return fmt.Errorf("error deleting metadata: %s", err)
+			}
+		}
+		if len(newMetadata) > 0 {
+			err := resource.MergeMetadata(types.MetadataStringValue, newMetadata)
+			if err != nil {
+				return fmt.Errorf("error adding metadata: %s", err)
+			}
+		}
+	}
+	return nil
+}
