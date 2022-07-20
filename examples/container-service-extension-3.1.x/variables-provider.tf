f706081a9a518e95106ce7a9b42ea76aa2a262bf
abarreiro <abarreiro@vmware.com>
2022-07-20 10:29:27 +0200

    Use alias
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- examples/container-service-extension-3.1.x/variables-provider.tf
+++ examples/container-service-extension-3.1.x/variables-provider.tf
@@ -1,7 +1,7 @@
 # These variables are for configuring the VCD provider
 
 variable "admin-user" {
-  description = "The System administrator user that will install CSE"
+  description = "The System administrator user that will create the CSE service account role and user"
   default     = "administrator"
   type        = string
 }
@@ -15,6 +15,19 @@ variable "admin-password" {
 variable "admin-org" {
   description = "The System administrator organization"
   default     = "System"
+  sensitive   = true
+  type        = string
+}
+
+variable "service-account-user" {
+  description = "The CSE service account user name that will install CSE. It will be created."
+  default     = "cse_service_account"
+  type        = string
+}
+
+variable "service-account-password" {
+  description = "The CSE service account password to put to the CSE service account."
+  default     = "cse_service_account"
   sensitive   = true
   type        = string
 }
