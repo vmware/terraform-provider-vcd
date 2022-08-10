b26021f51ce95debbc6d33c0f6f81b0bc4e64414
abarreiro <abarreiro@vmware.com>
2022-08-10 09:27:31 +0200

    Added filtering
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- vcd/catalogitem.go
+++ vcd/catalogitem.go
@@ -48,8 +48,7 @@ func deleteCatalogItem(d *schema.ResourceData, vcdClient *VCDClient) diag.Diagno
 }
 
 // Finds catalog item which can be vApp template OVA or media ISO file
-// TODO: This function should be updated in the context of Issue #502
-// Deprecated: Use findVappTemplate instead
+// Deprecated: This function is deprecated as described in Issue #502. Use findVappTemplate instead.
 func findCatalogItem(d *schema.ResourceData, vcdClient *VCDClient, origin string) (*govcd.CatalogItem, error) {
 	log.Printf("[TRACE] Catalog item read initiated")
 
@@ -107,6 +106,8 @@ func findCatalogItem(d *schema.ResourceData, vcdClient *VCDClient, origin string
 	return catalogItem, nil
 }
 
+// Finds a vApp Template with the information given in the resource data. If it's a data source it uses a filtering
+// mechanism, if it's a resource it just gets the information.
 func findVappTemplate(d *schema.ResourceData, vcdClient *VCDClient, origin string) (*govcd.VAppTemplate, error) {
 	log.Printf("[TRACE] vApp template search initiated")
 
@@ -129,39 +130,39 @@ func findVappTemplate(d *schema.ResourceData, vcdClient *VCDClient, origin strin
 		identifier = d.Get("name").(string)
 	}
 
-	var catalogItem *govcd.CatalogItem
+	var vAppTemplate *govcd.VAppTemplate
 	if origin == "datasource" {
 		if !nameOrFilterIsSet(d) {
-			return nil, fmt.Errorf(noNameOrFilterError, "vcd_catalog_item")
+			return nil, fmt.Errorf(noNameOrFilterError, "vcd_catalog_vapp_template")
 		}
 		filter, hasFilter := d.GetOk("filter")
 		if hasFilter {
 
-			catalogItem, err = getCatalogItemByFilter(catalog, filter, vcdClient.Client.IsSysAdmin)
+			vAppTemplate, err = getVappTemplateByFilter(catalog, filter, vcdClient.Client.IsSysAdmin)
 			if err != nil {
 				return nil, err
 			}
 
-			d.SetId(catalogItem.CatalogItem.ID)
-			return catalogItem, nil
+			d.SetId(vAppTemplate.VAppTemplate.ID)
+			return vAppTemplate, nil
 		}
 	}
 	// No filter: we continue with single item  GET
 
-	catalogItem, err = catalog.GetCatalogItemByNameOrId(identifier, false)
+	vAppTemplate, err = catalog.GetVappTemplateByNameOrId(identifier, false)
 	if govcd.IsNotFound(err) && origin == "resource" {
-		log.Printf("[INFO] Unable to find catalog item %s. Removing from tfstate", identifier)
+		log.Printf("[INFO] Unable to find vApp Template %s. Removing from tfstate", identifier)
 		d.SetId("")
 		return nil, nil
 	}
 
 	if err != nil {
-		return nil, fmt.Errorf("unable to find catalog item %s: %s", identifier, err)
+		return nil, fmt.Errorf("unable to find vApp Template %s: %s", identifier, err)
 	}
 
-	d.SetId(catalogItem.CatalogItem.ID)
-	log.Printf("[TRACE] Catalog item read completed: %#v", catalogItem.CatalogItem)
-	return catalogItem, nil
+	d.SetId(vAppTemplate.VAppTemplate.ID)
+	log.Printf("[TRACE] Catalog item read completed: %#v", vAppTemplate.VAppTemplate)
+	return vAppTemplate, nil
 }
 
 func getError(task govcd.UploadTask) error {
--- vcd/filter_get.go
+++ vcd/filter_get.go
@@ -60,7 +60,7 @@ func getCatalogByFilter(org *govcd.AdminOrg, filter interface{}, isSysAdmin bool
 }
 
 // getCatalogItemByFilter finds a catalog item using a filter block
-// TODO: This function should be updated in the context of Issue #502
+// Deprecated: This function is deprecated as described in Issue #502. Use getVappTemplateByFilter instead.
 func getCatalogItemByFilter(catalog *govcd.Catalog, filter interface{}, isSysAdmin bool) (*govcd.CatalogItem, error) {
 	queryType := types.QtVappTemplate
 	if isSysAdmin {
@@ -79,6 +79,28 @@ func getCatalogItemByFilter(catalog *govcd.Catalog, filter interface{}, isSysAdm
 		return nil, fmt.Errorf("[getCatalogItemByFilter] error retrieving catalog item %s: %s", queryItem.GetName(), err)
 	}
 	return catalogItem, nil
+}
+
+// getVappTemplateByFilter finds a vApp Template using a filter block.
+// This function deprecates getCatalogItemByFilter
+func getVappTemplateByFilter(catalog *govcd.Catalog, filter interface{}, isSysAdmin bool) (*govcd.VAppTemplate, error) {
+	queryType := types.QtVappTemplate
+	if isSysAdmin {
+		queryType = types.QtAdminVappTemplate
+	}
+	var searchFunc = func(queryType string, criteria *govcd.FilterDef) ([]govcd.QueryItem, string, error) {
+		return catalog.SearchByFilter(queryType, "catalogName", criteria)
+	}
+	queryItem, err := getEntityByFilter(searchFunc, queryType, "vApp Template", filter)
+	if err != nil {
+		return nil, err
+	}
+
+	vAppTemplate, err := catalog.GetVappTemplateByName(queryItem.GetName(), false)
+	if err != nil {
+		return nil, fmt.Errorf("[getVappTemplateByFilter] error retrieving vApp Template %s: %s", queryItem.GetName(), err)
+	}
+	return vAppTemplate, nil
 }
 
 // getMediaByFilter finds a media item using a filter block
