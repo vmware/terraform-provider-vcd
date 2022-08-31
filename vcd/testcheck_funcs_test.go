4f30f7b847162a9df4e121db8835d053f762f25c
abarreiro <abarreiro@vmware.com>
2022-08-31 15:45:48 +0200

    Add tag
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- vcd/testcheck_funcs_test.go
+++ vcd/testcheck_funcs_test.go
@@ -1,5 +1,5 @@
-//go:build vapp || vm || user || nsxt || extnetwork || network || gateway || catalog || standaloneVm || alb || vdcGroup || ALL || functional
-// +build vapp vm user nsxt extnetwork network gateway catalog standaloneVm alb vdcGroup ALL functional
+//go:build vapp || vm || user || nsxt || extnetwork || network || gateway || catalog || standaloneVm || alb || vdcGroup || ALL || vdc || functional
+// +build vapp vm user nsxt extnetwork network gateway catalog standaloneVm alb vdcGroup ALL vdc functional
 
 package vcd
 
