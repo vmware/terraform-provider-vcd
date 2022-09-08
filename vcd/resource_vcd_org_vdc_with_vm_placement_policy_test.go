824a22d667139834a649a90e6f46374ce585b62f
abarreiro <abarreiro@vmware.com>
2022-09-08 15:35:55 +0200

    Add removal of VM Placement Policies to VDC test
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- vcd/resource_vcd_org_vdc_with_vm_placement_policy_test.go
+++ vcd/resource_vcd_org_vdc_with_vm_placement_policy_test.go
@@ -33,6 +33,8 @@ func TestAccVcdOrgVdcWithVmPlacementPolicy(t *testing.T) {
 	configText := templateFill(testAccCheckVcdVdcVmPlacementPolicies_basic, params)
 	params["FuncName"] = t.Name() + "-Update"
 	updateText := templateFill(testAccCheckVcdVdcVmPlacementPolicies_update, params)
+	params["FuncName"] = t.Name() + "-Update2"
+	updateText2 := templateFill(testAccCheckVcdVdcVmPlacementPolicies_update2, params)
 
 	debugPrintf("#[DEBUG] CONFIGURATION - creation: %s", configText)
 	debugPrintf("#[DEBUG] CONFIGURATION - update: %s", updateText)
@@ -63,15 +65,15 @@ func TestAccVcdOrgVdcWithVmPlacementPolicy(t *testing.T) {
 			{
 				Config: updateText,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckVcdVdcExists(resourceName),
-					resource.TestCheckResourceAttr(resourceName, "name", t.Name()),
-					resource.TestCheckResourceAttr(resourceName, "org", testConfig.VCD.Org),
-					resource.TestCheckResourceAttr(resourceName, "network_pool_name", testConfig.VCD.NsxtProviderVdc.NetworkPool),
-					resource.TestCheckResourceAttr(resourceName, "provider_vdc_name", testConfig.VCD.NsxtProviderVdc.Name),
-					resource.TestCheckResourceAttr(resourceName, "enabled", "true"),
-					resource.TestCheckResourceAttr(resourceName, "storage_profile.0.name", testConfig.VCD.NsxtProviderVdc.StorageProfile),
 					resource.TestCheckResourceAttrPair(resourceName, "default_compute_policy_id", "vcd_vm_placement_policy.placement3", "id"),
 					resource.TestCheckResourceAttr(resourceName, "vm_placement_policy_ids.#", "2"),
+				),
+			},
+			{
+				Config: updateText2,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(resourceName, "default_compute_policy_id", "vcd_vm_sizing_policy.dummy", "id"),
+					resource.TestCheckResourceAttr(resourceName, "vm_placement_policy_ids.#", "0"),
 				),
 			},
 			{
@@ -223,5 +225,81 @@ resource "vcd_org_vdc" "{{.VdcName}}" {
 
   default_compute_policy_id = vcd_vm_placement_policy.placement3.id
   vm_placement_policy_ids   = [vcd_vm_placement_policy.placement1.id, vcd_vm_placement_policy.placement3.id]
+}
+`
+
+// In this one we remove the VM Placement Policies
+const testAccCheckVcdVdcVmPlacementPolicies_update2 = `
+# skip-binary-test: only for updates
+data "vcd_provider_vdc" "pvdc" {
+  name = "{{.ProviderVdc}}"
+}
+
+data "vcd_vm_group" "vm-group" {
+  name            = "{{.VmGroup}}"
+  provider_vdc_id = data.vcd_provider_vdc.pvdc.id
+}
+
+resource "vcd_vm_sizing_policy" "dummy" {
+  name        = "dummy"
+  description = "dummy description"
+}
+
+resource "vcd_vm_placement_policy" "placement1" {
+  name        = "placement1"
+  provider_vdc_id = data.vcd_provider_vdc.pvdc.id
+  description = "placement1 description"
+  vm_group_ids = [ data.vcd_vm_group.vm-group.id ]
+}
+
+resource "vcd_vm_placement_policy" "placement2" {
+  name        = "placement2"
+  provider_vdc_id = data.vcd_provider_vdc.pvdc.id
+  description = "placement2 description"
+  vm_group_ids = [ data.vcd_vm_group.vm-group.id ]
+}
+
+resource "vcd_vm_placement_policy" "placement3" {
+  name        = "placement3"
+  provider_vdc_id = data.vcd_provider_vdc.pvdc.id
+  description = "placement3 description"
+  vm_group_ids = [ data.vcd_vm_group.vm-group.id ]
+}
+
+resource "vcd_org_vdc" "{{.VdcName}}" {
+  name = "{{.VdcName}}"
+  org  = "{{.OrgName}}"
+
+  allocation_model  = "ReservationPool"
+  network_pool_name = "{{.NetworkPool}}"
+  provider_vdc_name = data.vcd_provider_vdc.pvdc.name
+
+  compute_capacity {
+    cpu {
+      allocated = 1024
+      limit     = 1024
+    }
+
+    memory {
+      allocated = 1024
+      limit     = 1024
+    }
+  }
+
+  storage_profile {
+    name     = "{{.ProviderVdcStorageProfile}}"
+    enabled  = true
+    limit    = 10240
+    default  = true
+  }
+
+  enabled                    = true
+  enable_thin_provisioning   = true
+  enable_fast_provisioning   = true
+  delete_force               = true
+  delete_recursive           = true
+
+  default_compute_policy_id = vcd_vm_sizing_policy.dummy.id # As this can't be empty
+  vm_placement_policy_ids   = []
 }
 `
