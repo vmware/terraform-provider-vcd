88e729187cedf5c4ec28ade4528ab3e0cacfb0f5
abarreiro <abarreiro@vmware.com>
2022-09-09 12:14:06 +0200

    Fixed contains
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- vcd/structure.go
+++ vcd/structure.go
@@ -190,9 +190,6 @@ func convertSliceOfStringsToOpenApiReferenceIds(ids []string) []types.OpenApiRef
 
 // contains returns true if `sliceToSearch` contains `searched`. Returns false otherwise.
 func contains(sliceToSearch []string, searched string) bool {
-	if searched == "" && len(sliceToSearch) == 0 {
-		return true
-	}
 	found := false
 	for _, idInSlice := range sliceToSearch {
 		if searched == idInSlice {
