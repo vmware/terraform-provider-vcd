81b2f9603e50500beb2949d1fbd2797e3a3c3c5e
abarreiro <abarreiro@vmware.com>
2022-09-28 12:08:51 +0200

    Add script to check go.mod
    Signed-off-by: abarreiro <abarreiro@vmware.com>

---
+++ scripts/check-gomod.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This script is intended to check whether go.mod has a replaced module, exiting with error if so.
+# If the environment variable RELEASE is set, it also checks whether go-vcloud-director is used a released version.
+
+GO_VCLOUD_DIRECTOR_URI='github.com/vmware/go-vcloud-director/v2'
+
+if [ ! -f go.mod ]
+then
+    echo "go.mod file doesn't exist"
+    exit 1
+fi
+
+grepAlpha=''
+if [ -n "$RELEASE" ]; then
+  echo "INFO: Will check that no unreleased versions of go-vcloud-director are present in go.mod"
+  grepAlpha=$(grep -E "$GO_VCLOUD_DIRECTOR_URI v[1-9]+\.[0-9]+\.[0-9]+\-.*" go.mod)
+  if [ -n "$grepAlpha" ]
+  then
+    >&2 printf "ERROR: Found a non-released version of go-vcloud-director: %s\n" "$(echo "$grepAlpha" | cut -d' ' -f 2)"
+  fi
+fi
+
+grepReplace=$(grep "replace $GO_VCLOUD_DIRECTOR_URI" go.mod | grep -v '//')
+if [ -n "$grepReplace" ]; then
+  >&2 printf "ERROR: Found these replaced modules:\n%s\n" "$(echo "$grepReplace" | cut -d' ' -f 2)"
+fi
+
+if [ -z "$grepAlpha" ] && [ -z "$grepReplace" ]; then
+  echo "No errors found: SUCCESS"
+  exit 0
+fi
+
+echo "Errors found: FAILURE"
+exit 1
