6440a891d3f34c5e691421598738c18339a8f04c
abarreiro <abarreiro@vmware.com>
2022-10-19 14:25:48 +0200

    Remove comment
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- vcd/filter.go
+++ vcd/filter.go
@@ -126,7 +126,6 @@ func buildMetadataCriteria(metadataBlock interface{}) ([]govcd.MetadataDef, bool
 			case "use_api_search":
 				useApiSearch = value.(bool)
 			}
-			// TODO: Add "visibility"???
 		}
 		definitions = append(definitions, def)
 	}
