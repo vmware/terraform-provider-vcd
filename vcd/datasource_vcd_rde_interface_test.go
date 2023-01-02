0e7cbcc4cf6c8e009bbddfbb01680b85bab1d01b
abarreiro <abarreiro@vmware.com>
2023-01-02 13:19:47 +0100

    Add data source test
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- vcd/datasource_vcd_rde_interface_test.go
+++ vcd/datasource_vcd_rde_interface_test.go
@@ -13,7 +13,7 @@ func TestAccVcdRdeDefinedInterfaceDS(t *testing.T) {
 	preTestChecks(t)
 	skipIfNotSysAdmin(t)
 
-	// This is an Defined Interface that comes with VCD out of the box
+	// This is a Defined Interface that comes with VCD out of the box
 	var params = StringMap{
 		"Namespace": "k8s",
 		"Version":   "1.0.0",
@@ -39,7 +39,7 @@ func TestAccVcdRdeDefinedInterfaceDS(t *testing.T) {
 					resource.TestCheckResourceAttr(interfaceName, "namespace", params["Namespace"].(string)),
 					resource.TestCheckResourceAttr(interfaceName, "version", params["Version"].(string)),
 					resource.TestCheckResourceAttr(interfaceName, "vendor", params["Vendor"].(string)),
-					resource.TestCheckResourceAttr(interfaceName, "name", "Kubernetes"), // Name is always Kubernetes
+					resource.TestCheckResourceAttr(interfaceName, "name", "Kubernetes"), // Name is always the same
 					resource.TestCheckResourceAttr(interfaceName, "id", fmt.Sprintf("urn:vcloud:interface:%s:%s:%s", params["Vendor"].(string), params["Namespace"].(string), params["Version"].(string))),
 					resource.TestCheckResourceAttr(interfaceName, "readonly", "false"),
 				),
