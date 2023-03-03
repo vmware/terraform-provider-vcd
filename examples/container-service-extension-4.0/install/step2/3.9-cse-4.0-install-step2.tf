a9824692bef2fd03e03331851b389bf0337ab7fd
abarreiro <abarreiro@vmware.com>
2023-03-03 14:42:40 +0100

    fmt
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- examples/container-service-extension-4.0/install/step2/3.9-cse-4.0-install-step2.tf
+++ examples/container-service-extension-4.0/install/step2/3.9-cse-4.0-install-step2.tf
@@ -661,7 +661,7 @@ resource "vcd_rde" "vcdkeconfig_instance" {
   rde_type_nss     = data.vcd_rde_type.existing_vcdkeconfig_type.nss
   rde_type_version = data.vcd_rde_type.existing_vcdkeconfig_type.version
   resolve          = true
-  input_entity     = templatefile(var.vcdkeconfig_template_filepath, {
+  input_entity = templatefile(var.vcdkeconfig_template_filepath, {
     capvcd_version                  = var.capvcd_version
     cpi_version                     = var.cpi_version
     csi_version                     = var.csi_version
