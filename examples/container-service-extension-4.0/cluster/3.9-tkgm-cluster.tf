1a2776a6594bcbb333ff0f7bcae9a90e03e56a95
abarreiro <abarreiro@vmware.com>
2023-03-09 16:12:50 +0100

    Add example
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- examples/container-service-extension-4.0/cluster/3.9-tkgm-cluster.tf
+++ examples/container-service-extension-4.0/cluster/3.9-tkgm-cluster.tf
@@ -4,15 +4,6 @@
 # * Please read the guide present at https://registry.terraform.io/providers/vmware/vcd/latest/docs/guides/container_service_extension_4_0
 #   before applying this configuration.
 #
-# * The installation process is split into two steps as Providers will need to generate an API token for the created
-#   CSE administrator user, in order to use it with the CSE Server that will be deployed in the second step.
-#
-# * This step will only create the required Runtime Defined Entity (RDE) Interfaces, Types, Role and finally
-#   the CSE administrator user.
-#
-# * Rename "terraform.tfvars.example" to "terraform.tfvars" and adapt the values to your needs.
-#   Other than that, this snippet should be applied as it is.
-#   You can check the comments on each resource/data source for more help and context.
 # ------------------------------------------------------------------------------------------------------------
 
 # VCD Provider configuration. It must be at least v3.9.0 and configured with a System administrator account.
@@ -30,15 +21,51 @@ provider "vcd" {
   user                 = var.administrator_user
   password             = var.administrator_password
   auth_type            = "integrated"
-  sysorg               = var.administrator_org
-  org                  = var.administrator_org
+  sysorg               = "System"
+  org                  = "cluster_org"
   allow_unverified_ssl = var.insecure_login
   logging              = true
-  logging_file         = "cse_install_step1.log"
+  logging_file         = "cluster_${var.administrator_user}.log"
+}
+
+data "vcd_vm_sizing_policy" "cluster_sizing_policy" {
+  name = "TKG small"
+}
+
+data "vcd_catalog" "cluster_catalog" {
+  org  = "solutions_org"
+  name = "tkgm_catalog"
+}
+
+data "vcd_catalog_vapp_template" "cluster_tkgm_ova" {
+  catalog_id = data.vcd_catalog.cluster_catalog.id
+  name       = "ubuntu-2004-kube-v1.22.9+vmware.1-tkg.1-2182cbabee08edf480ee9bc5866d6933"
+}
+
+data "vcd_role" "cluster_author_role" {
+  name = "Kubernetes Cluster Author"
+}
+
+resource "vcd_org_user" "cluster_author" {
+  name     = "cluster_author"
+  password = "change-me"
+  role     = data.vcd_role.cluster_author_role.name
+}
+
+provider "vcd" {
+  alias                = "cluster_author"
+  url                  = "${var.vcd_url}/api"
+  user                 = vcd_org_user.cluster_author.name
+  password             = vcd_org_user.cluster_author.password
+  auth_type            = "integrated"
+  org                  = "cluster_org"
+  allow_unverified_ssl = var.insecure_login
+  logging              = true
+  logging_file         = "cluster_${vcd_org_user.cluster_author.name}.log"
 }
 
 locals {
-  capvcd_yaml_rendered = templatefile("/users/bob/capvcd-templates/cluster-template-v1.22.9.yaml", {
+  capvcd_yaml_rendered = templatefile("./cluster-template-v1.22.9.yaml", {
     CLUSTER_NAME     = "my-cluster"
     TARGET_NAMESPACE = "my-cluster-ns"
 
@@ -47,23 +74,24 @@ locals {
     VCD_ORGANIZATION_VDC         = "cluster_vdc"
     VCD_ORGANIZATION_VDC_NETWORK = "cluster_routed_network"
 
-    VCD_USERNAME_B64      = base64encode(var.k8s_cluster_user)
-    VCD_REFRESH_TOKEN_B64 = base64encode(var.k8s_cluster_api_token)
+    VCD_USERNAME_B64      = vcd_org_user.cluster_author.name
+    VCD_PASSWORD_B64      = vcd_org_user.cluster_author.password
+    VCD_REFRESH_TOKEN_B64 = ""
     SSH_PUBLIC_KEY        = ""
 
     CONTROL_PLANE_MACHINE_COUNT        = 1
-    VCD_CONTROL_PLANE_SIZING_POLICY    =
+    VCD_CONTROL_PLANE_SIZING_POLICY    = data.vcd_vm_sizing_policy.cluster_sizing_policy.name
     VCD_CONTROL_PLANE_PLACEMENT_POLICY = ""
     VCD_CONTROL_PLANE_STORAGE_PROFILE  = ""
 
     VCD_WORKER_STORAGE_PROFILE  = ""
-    VCD_WORKER_SIZING_POLICY    = vcd_vm_sizing_policy.default_policy.name
+    VCD_WORKER_SIZING_POLICY    = data.vcd_vm_sizing_policy.cluster_sizing_policy.name
     VCD_WORKER_PLACEMENT_POLICY = ""
     WORKER_MACHINE_COUNT        = 1
 
     DISK_SIZE         = "20Gi"
-    VCD_CATALOG       = vcd_catalog.cse_catalog.name
-    VCD_TEMPLATE_NAME = replace(var.tkgm_ova_name, ".ova", "")
+    VCD_CATALOG       = data.vcd_catalog.cluster_catalog.name
+    VCD_TEMPLATE_NAME = data.vcd_catalog_vapp_template.cluster_tkgm_ova.name
 
     POD_CIDR     = "100.96.0.0/11"
     SERVICE_CIDR = "100.64.0.0/13"
