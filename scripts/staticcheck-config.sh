cfaf8fdf2f2960a419f11fccfd7ac65f38479009
Dainius Serplis <dserplis@vmware.com>
2023-03-27 09:55:53 +0300

    Fix staticcheck version url
    Signed-off-by: Dainius Serplis <dserplis@vmware.com>

--- scripts/staticcheck-config.sh
+++ scripts/staticcheck-config.sh
@@ -1,4 +1,3 @@
 export STATICCHECK_URL=https://github.com/dominikh/go-tools/releases/download
-export STATICCHECK_VERSION=v0.4.3
+export STATICCHECK_VERSION=2023.1.3
 export STATICCHECK_FILE=staticcheck_linux_amd64.tar.gz
-
