82b666dd5abf5de78188030537eb9d9e5ce64ce1
abarreiro <abarreiro@vmware.com>
2023-03-30 11:23:25 +0200

    Change Cluster Org to Tenant Org
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- examples/container-service-extension-4.0/install/step2/3.9-cse-4.0-install-step2.tf
+++ examples/container-service-extension-4.0/install/step2/3.9-cse-4.0-install-step2.tf
@@ -64,12 +64,12 @@ resource "vcd_org" "solutions_organization" {
   }
 }
 
-# The Cluster Organization will host the TKGm clusters and its intended to be used by tenants.
+# The Tenant Organization will host the TKGm clusters and its intended to be used by tenants.
 # The TKGm clusters must be placed here. The attributes related to lease are set to unlimited, as the TKGm clusters vApps
 # should not be powered off.
-resource "vcd_org" "cluster_organization" {
-  name             = "cluster_org"
-  full_name        = "Cluster Organization"
+resource "vcd_org" "tenant_organization" {
+  name             = "tenant_org"
+  full_name        = "Tenant Organization"
   is_enabled       = true
   delete_force     = true
   delete_recursive = true
@@ -139,22 +139,22 @@ data "vcd_provider_vdc" "nsxt_pvdc" {
   name = var.provider_vdc_name
 }
 
-data "vcd_nsxt_edge_cluster" "cluster_edgecluster" {
-  org             = vcd_org.cluster_organization.name
+data "vcd_nsxt_edge_cluster" "nsxt_edgecluster" {
+  org             = vcd_org.tenant_organization.name
   provider_vdc_id = data.vcd_provider_vdc.nsxt_pvdc.id
   name            = var.nsxt_edge_cluster_name
 }
 
 # The VDC that will host the Kubernetes clusters.
-resource "vcd_org_vdc" "cluster_vdc" {
-  name        = "cluster_vdc"
-  description = "Cluster VDC"
-  org         = vcd_org.cluster_organization.name
+resource "vcd_org_vdc" "tenant_vdc" {
+  name        = "tenant_vdc"
+  description = "Tenant VDC"
+  org         = vcd_org.tenant_organization.name
 
   allocation_model  = "AllocationVApp" # You can use other models.
   network_pool_name = var.network_pool_name
   provider_vdc_name = data.vcd_provider_vdc.nsxt_pvdc.name
-  edge_cluster_id   = data.vcd_nsxt_edge_cluster.cluster_edgecluster.id
+  edge_cluster_id   = data.vcd_nsxt_edge_cluster.nsxt_edgecluster.id
 
   # You can tune these arguments to your fit your needs.
   network_quota = 50
@@ -201,7 +201,7 @@ resource "vcd_org_vdc" "solutions_vdc" {
   allocation_model  = "AllocationVApp" # You can use other models
   network_pool_name = var.network_pool_name
   provider_vdc_name = data.vcd_provider_vdc.nsxt_pvdc.name
-  edge_cluster_id   = data.vcd_nsxt_edge_cluster.cluster_edgecluster.id
+  edge_cluster_id   = data.vcd_nsxt_edge_cluster.nsxt_edgecluster.id
 
   # You can tune these arguments to your fit your needs
   network_quota = 10
@@ -261,13 +261,13 @@ resource "vcd_catalog" "tkgm_catalog" {
   ]
 }
 
-# We share the TKGm Catalog with the Cluster Organization created previously.
+# We share the TKGm Catalog with the Tenant Organization created previously.
 resource "vcd_catalog_access_control" "tkgm_catalog_ac" {
   org                  = vcd_org.solutions_organization.name # References the Solutions Organization created previously
   catalog_id           = vcd_catalog.tkgm_catalog.id
   shared_with_everyone = false
   shared_with {
-    org_id       = vcd_org.cluster_organization.id # Shared with the Cluster Organization
+    org_id       = vcd_org.tenant_organization.id # Shared with the Tenant Organization
     access_level = "ReadOnly"
   }
 }
@@ -443,25 +443,25 @@ resource "vcd_external_network_v2" "solutions_tier0" {
   }
 }
 
-data "vcd_nsxt_tier0_router" "cluster_tier0_router" {
-  name            = var.cluster_nsxt_tier0_router_name
+data "vcd_nsxt_tier0_router" "tenant_tier0_router" {
+  name            = var.tenant_nsxt_tier0_router_name
   nsxt_manager_id = data.vcd_nsxt_manager.cse_nsxt_manager.id
 }
 
-resource "vcd_external_network_v2" "cluster_tier0" {
-  name = "cluster_tier0"
+resource "vcd_external_network_v2" "tenant_tier0" {
+  name = "tenant_tier0"
 
   nsxt_network {
     nsxt_manager_id      = data.vcd_nsxt_manager.cse_nsxt_manager.id
-    nsxt_tier0_router_id = data.vcd_nsxt_tier0_router.cluster_tier0_router.id
+    nsxt_tier0_router_id = data.vcd_nsxt_tier0_router.tenant_tier0_router.id
   }
 
   ip_scope {
-    gateway       = var.cluster_provider_gateway_gateway_ip
-    prefix_length = var.cluster_provider_gateway_gateway_prefix_length
+    gateway       = var.tenant_provider_gateway_gateway_ip
+    prefix_length = var.tenant_provider_gateway_gateway_prefix_length
 
     dynamic "static_ip_pool" {
-      for_each = var.cluster_provider_gateway_static_ip_ranges
+      for_each = var.tenant_provider_gateway_static_ip_ranges
       iterator = ip
       content {
         start_address = ip.value[0]
@@ -496,20 +496,20 @@ resource "vcd_nsxt_edgegateway" "solutions_edgegateway" {
 }
 
 # This Edge Gateway will consume automatically the available IPs from the Provider Gateway.
-resource "vcd_nsxt_edgegateway" "cluster_edgegateway" {
-  org      = vcd_org.cluster_organization.name
-  owner_id = vcd_org_vdc.cluster_vdc.id
+resource "vcd_nsxt_edgegateway" "tenant_edgegateway" {
+  org      = vcd_org.tenant_organization.name
+  owner_id = vcd_org_vdc.tenant_vdc.id
 
-  name                = "cluster_edgegateway"
-  external_network_id = vcd_external_network_v2.cluster_tier0.id
+  name                = "tenant_edgegateway"
+  external_network_id = vcd_external_network_v2.tenant_tier0.id
 
   subnet {
-    gateway       = var.cluster_provider_gateway_gateway_ip
-    prefix_length = var.cluster_provider_gateway_gateway_prefix_length
-    primary_ip    = var.cluster_provider_gateway_static_ip_ranges[0][0]
+    gateway       = var.tenant_provider_gateway_gateway_ip
+    prefix_length = var.tenant_provider_gateway_gateway_prefix_length
+    primary_ip    = var.tenant_provider_gateway_static_ip_ranges[0][0]
 
     dynamic "allocated_ips" {
-      for_each = var.cluster_provider_gateway_static_ip_ranges
+      for_each = var.tenant_provider_gateway_static_ip_ranges
       iterator = ip
       content {
         start_address = ip.value[0]
@@ -565,18 +565,18 @@ resource "vcd_nsxt_alb_edgegateway_service_engine_group" "solutions_assignment" 
   max_virtual_services      = 50
 }
 
-resource "vcd_nsxt_alb_edgegateway_service_engine_group" "cluster_assignment" {
-  org                       = vcd_org.cluster_organization.name
-  edge_gateway_id           = vcd_nsxt_alb_settings.cluster_alb_settings.edge_gateway_id
+resource "vcd_nsxt_alb_edgegateway_service_engine_group" "tenant_assignment" {
+  org                       = vcd_org.tenant_organization.name
+  edge_gateway_id           = vcd_nsxt_alb_settings.tenant_alb_settings.edge_gateway_id
   service_engine_group_id   = vcd_nsxt_alb_service_engine_group.cse_alb_seg.id
   reserved_virtual_services = 50
   max_virtual_services      = 50
 }
 
-## ALB for cluster edge gateway
-resource "vcd_nsxt_alb_settings" "cluster_alb_settings" {
-  org             = vcd_org.cluster_organization.name
-  edge_gateway_id = vcd_nsxt_edgegateway.cluster_edgegateway.id
+## ALB for tenant edge gateway
+resource "vcd_nsxt_alb_settings" "tenant_alb_settings" {
+  org             = vcd_org.tenant_organization.name
+  edge_gateway_id = vcd_nsxt_edgegateway.tenant_edgegateway.id
   is_active       = true
 
   depends_on = [vcd_nsxt_alb_service_engine_group.cse_alb_seg]
@@ -602,24 +602,24 @@ resource "vcd_network_routed_v2" "solutions_routed_network" {
   dns_suffix = var.solutions_routed_network_dns_suffix
 }
 
-# We create a Routed network in the Cluster organization that will be used by the Kubernetes clusters.
-resource "vcd_network_routed_v2" "cluster_routed_network" {
-  org         = vcd_org.cluster_organization.name
-  name        = "cluster_net_routed"
+# We create a Routed network in the Tenant organization that will be used by the Kubernetes clusters.
+resource "vcd_network_routed_v2" "tenant_routed_network" {
+  org         = vcd_org.tenant_organization.name
+  name        = "tenant_net_routed"
   description = "Routed network for the K8s clusters"
 
-  edge_gateway_id = vcd_nsxt_edgegateway.cluster_edgegateway.id
+  edge_gateway_id = vcd_nsxt_edgegateway.tenant_edgegateway.id
 
-  gateway       = var.cluster_routed_network_gateway_ip
-  prefix_length = var.cluster_routed_network_prefix_length
+  gateway       = var.tenant_routed_network_gateway_ip
+  prefix_length = var.tenant_routed_network_prefix_length
 
   static_ip_pool {
-    start_address = var.cluster_routed_network_ip_pool_start_address
-    end_address   = var.cluster_routed_network_ip_pool_end_address
+    start_address = var.tenant_routed_network_ip_pool_start_address
+    end_address   = var.tenant_routed_network_ip_pool_end_address
   }
 
-  dns1       = var.cluster_routed_network_dns
-  dns_suffix = var.cluster_routed_network_dns_suffix
+  dns1       = var.tenant_routed_network_dns
+  dns_suffix = var.tenant_routed_network_dns_suffix
 }
 
 # We need SNAT rules in both networks to provide with Internet connectivity.
@@ -636,16 +636,16 @@ resource "vcd_nsxt_nat_rule" "solutions_nat" {
   logging          = true
 }
 
-resource "vcd_nsxt_nat_rule" "cluster_nat" {
+resource "vcd_nsxt_nat_rule" "tenant_nat" {
   org             = vcd_org.solutions_organization.name
-  edge_gateway_id = vcd_nsxt_edgegateway.cluster_edgegateway.id
+  edge_gateway_id = vcd_nsxt_edgegateway.tenant_edgegateway.id
 
-  name        = "Cluster SNAT rule"
+  name        = "Tenant SNAT rule"
   rule_type   = "SNAT"
-  description = "Cluster SNAT rule"
+  description = "Tenant SNAT rule"
 
-  external_address = var.cluster_snat_external_ip
-  internal_address = var.cluster_snat_internal_network_cidr
+  external_address = var.tenant_snat_external_ip
+  internal_address = var.tenant_snat_internal_network_cidr
   logging          = true
 }
 
@@ -663,9 +663,9 @@ resource "vcd_nsxt_firewall" "solutions_firewall" {
 }
 
 # WARNING: Please adjust this rule to your needs. The Bootstrap clusters and final Kubernetes clusters require Internet access to be configured.
-resource "vcd_nsxt_firewall" "cluster_firewall" {
-  org             = vcd_org.cluster_organization.name
-  edge_gateway_id = vcd_nsxt_edgegateway.cluster_edgegateway.id
+resource "vcd_nsxt_firewall" "tenant_firewall" {
+  org             = vcd_org.tenant_organization.name
+  edge_gateway_id = vcd_nsxt_edgegateway.tenant_edgegateway.id
 
   rule {
     action      = "ALLOW"
