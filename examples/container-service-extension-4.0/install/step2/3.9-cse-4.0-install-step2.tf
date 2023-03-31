a251daed0708d7494fe4118ceb99c239d8b18edc
abarreiro <abarreiro@vmware.com>
2023-03-31 11:50:56 +0200

    nit
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- examples/container-service-extension-4.0/install/step2/3.9-cse-4.0-install-step2.tf
+++ examples/container-service-extension-4.0/install/step2/3.9-cse-4.0-install-step2.tf
@@ -767,5 +767,5 @@ resource "vcd_vapp_vm" "cse_server_vm" {
 }
 
 output "publish_ui_plugin" {
-  value = "When CSE Server ${vcd_vapp_vm.cse_server_vm.name} is ready, please install the Kubernetes Container Clusters UI plug-in 4.0 for VCD that you can download from https://docs.vmware.com/en/VMware-Cloud-Director-Container-Service-Extension/index.html"
+  value = "When CSE Server '${vcd_vapp_vm.cse_server_vm.name}' is ready, please install the Kubernetes Container Clusters UI plug-in 4.0 for VCD that you can download from https://docs.vmware.com/en/VMware-Cloud-Director-Container-Service-Extension/index.html"
 }
