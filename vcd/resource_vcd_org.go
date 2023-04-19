7e05bd935df2e373d8a43bd4e4085d2afcda1bc3
Dainius Serplis <dserplis@vmware.com>
2023-04-19 07:23:19 +0300

    Improve error message wording
    Signed-off-by: Dainius Serplis <dserplis@vmware.com>

--- vcd/resource_vcd_org.go
+++ vcd/resource_vcd_org.go
@@ -202,7 +202,7 @@ func resourceOrgCreate(ctx context.Context, d *schema.ResourceData, m interface{
 			// The error itself is SQL message therefore we override it
 			vcdVersion, _, _ := vcdClient.Client.GetVcdVersion()
 			err = fmt.Errorf("\nthis version of VCD (%s) has a bug that prevents creating a disabled Org\n"+
-				"If you need to disable, please create an enabled Org and then disable it using update", vcdVersion)
+				"If you need to disable it, please create an enabled Org and then disable it using update", vcdVersion)
 		}
 
 		return diag.Errorf("[org creation] error creating Org %s: %s", orgName, err)
