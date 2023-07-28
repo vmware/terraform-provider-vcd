b82af01a899a11563ee0853a477ba5d41e580cfc
Giuseppe Maxia <gmaxia@vmware.com>
2023-07-28 11:25:48 +0200

    Add function testAccCheckOrgDestroy
    Signed-off-by: Giuseppe Maxia <gmaxia@vmware.com>

--- vcd/common_test.go
+++ vcd/common_test.go
@@ -1,8 +1,15 @@
-//go:build network || nsxt || gateway || ALL || functional
+//go:build network || nsxt || gateway || org || catalog || ALL || functional
 
 package vcd
 
-import "testing"
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/vmware/go-vcloud-director/v2/govcd"
+	"testing"
+	"time"
+)
 
 // updateEdgeGatewayTier0Dedication updates default NSX-T Edge Gateway used for tests to use Tier-0
 // gateway as dedicated for the purpose of some tests
@@ -21,5 +28,28 @@ func updateEdgeGatewayTier0Dedication(t *testing.T, dedicatedTier0 bool) {
 	_, err = edge.Update(edge.EdgeGateway)
 	if err != nil {
 		t.Fatalf("error updating NSX-T Edge Gateway dedicated Tier 0 gateway usage to '%t': %s", dedicatedTier0, err)
+	}
+}
+
+// TODO investigate the need for delay on Org removal
+func testAccCheckOrgDestroy(orgName string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		conn := testAccProvider.Meta().(*VCDClient)
+		var org *govcd.AdminOrg
+		var err error
+		for N := 0; N < 30; N++ {
+			org, err = conn.GetAdminOrgByName(orgName)
+			if err != nil && org == nil {
+				break
+			}
+			time.Sleep(time.Second)
+		}
+		if err != govcd.ErrorEntityNotFound {
+			return fmt.Errorf("org %s was not destroyed", orgName)
+		}
+		if org != nil {
+			return fmt.Errorf("org %s was found", orgName)
+		}
+		return nil
 	}
 }
