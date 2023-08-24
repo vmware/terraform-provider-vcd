21d205c03cbb432426077d7e041959b3532397d3
Giuseppe Maxia <gmaxia@vmware.com>
2023-08-24 21:42:19 +0200

    Add files with test lists for partition monitoring
    Signed-off-by: Giuseppe Maxia <gmaxia@vmware.com>

--- vcd/partitioning_test.go
+++ vcd/partitioning_test.go
@@ -1,6 +1,7 @@
 package vcd
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"regexp"
@@ -33,6 +34,26 @@ var (
 
 	// testMapMx is a mutex that controls the mapOfTests access
 	testMapMx sync.Mutex
+
+	// testListMx is a mutex that controls access to the file of processed tests
+	testListMx sync.Mutex
+
+	// listOfTestForNode contains the tests for the current node
+	listOfTestForNode []string
+
+	// listOfProcessedTests contains the list of tests processed by the partitioned node
+	listOfProcessedTests []string
+
+	// notPartitionedTests are the tests that have side effects and should not be partitioned
+	notPartitionedTests = []string{
+		"TestMain",               // mother of all tests. Can't be split
+		"TestTags",               // only runs if the suite is called without tags
+		"TestProvider",           // used to create provider
+		"TestProvider_impl",      // used to define provider
+		"TestCustomTemplates",    // used to fill binary tests
+		"TestAccClientUserAgent", // sets user agent
+		"TestAccVcdVAppRawMulti", // very old experimental test that should be removed
+	}
 )
 
 // getListOfTests retrieves the list of tests from the current directory
@@ -64,6 +85,9 @@ func getListOfTests() []string {
 		for _, fn := range testNames {
 			// keeps only the test name
 			testName := strings.Replace(string(fn), "func ", "", 1)
+			if contains(notPartitionedTests, testName) {
+				continue
+			}
 			testList = append(testList, testName)
 		}
 	}
@@ -94,9 +118,6 @@ func getMapOfTests() map[string]partitionInfo {
 	nodeNumber := 0
 	var testMap = make(map[string]partitionInfo)
 	for _, tn := range listOfTests {
-		if tn == "TestMain" {
-			continue
-		}
 		// Every test gets assigned a number
 		testNumber++
 
@@ -105,31 +126,77 @@ func getMapOfTests() map[string]partitionInfo {
 		if nodeNumber > numberOfPartitions {
 			nodeNumber = 1
 		}
+		if nodeNumber == partitionNode {
+			listOfTestForNode = append(listOfTestForNode, tn)
+		}
 		testMap[tn] = partitionInfo{
 			index: testNumber,
 			node:  nodeNumber,
 		}
 	}
+	fileName := fmt.Sprintf("tests-retrieved-in-node-%d-%s.txt", partitionNode, testConfig.Provider.VcdVersion)
+	_ = os.WriteFile(fileName, []byte(strings.Join(listOfTests, "\n")), 0600)
+
+	fileName = fmt.Sprintf("tests-planned-in-node-%d-%s.txt", partitionNode, testConfig.Provider.VcdVersion)
+	var testList strings.Builder
+	testList.WriteString(fmt.Sprintf("# VCD: %s \n", strings.Replace(testConfig.Provider.Url, "/api", "", 1)))
+	testList.WriteString(fmt.Sprintf("# version: %s \n", testConfig.Provider.VcdVersion))
+	testList.WriteString(fmt.Sprintf("# node N.: %d \n", partitionNode))
+	for _, tn := range listOfTestForNode {
+		testList.WriteString(fmt.Sprintf("%s\n", tn))
+	}
+	_ = os.WriteFile(fileName, []byte(testList.String()), 0600)
 	return testMap
 }
 
+func writeProcessedTests(fileName, line string) {
+	testListMx.Lock()
+	defer testListMx.Unlock()
+	var fileHandler *os.File
+	var err error
+	if fileExists(fileName) {
+		fileHandler, err = os.OpenFile(fileName, os.O_WRONLY|os.O_APPEND, os.ModeAppend)
+	} else {
+		fileHandler, err = os.Create(fileName)
+	}
+	if err != nil {
+		fmt.Printf("##### ERROR opening file %s : %s\n", fileName, err)
+		os.Exit(1)
+	}
+	defer safeClose(fileHandler)
+	w := bufio.NewWriter(fileHandler)
+	_, err = fmt.Fprintln(w, line)
+	if err != nil {
+		fmt.Printf("error writing to file %s: %s\n", fileName, err)
+		os.Exit(1)
+	}
+	_ = w.Flush()
+}
+
 func handlePartitioning(t *testing.T) {
+	// If partitioning is not enabled we bail out
 	if numberOfPartitions == 0 {
 		return
 	}
+	// Number of partitions should be at least 2
 	if numberOfPartitions == 1 {
 		fmt.Printf("number of partitions (-vcd-partitions) must be greater than 1\n")
 		os.Exit(1)
 	}
+
+	// When partitioning is enabled, we must have a node identified for the current run
 	if partitionNode == 0 {
 		fmt.Printf("number of partitions (-vcd-partitions) was set, but not the partition node (-vcd-partition-node)\n")
 		os.Exit(1)
 	}
+
+	// The current node can't be higher than the number of partitions
 	if partitionNode > numberOfPartitions {
 		fmt.Printf("partition node (%d) is bigger than number of partitions (%d)\n", partitionNode, numberOfPartitions)
 		os.Exit(1)
 	}
 	testName := t.Name()
+	// If this is the first test being processed, we collect the list of tests
 	if len(mapOfTests) == 0 {
 		mapOfTests = getMapOfTests()
 	}
@@ -144,7 +211,21 @@ func handlePartitioning(t *testing.T) {
 	}
 
 	if partInfo.node == partitionNode {
-		fmt.Printf("[partitioning] [%d %s]\n", partInfo.index, testName)
+		fileName := fmt.Sprintf("tests-seen-in-node-%d-%s.txt", partitionNode, testConfig.Provider.VcdVersion)
+		if len(listOfProcessedTests) == 0 {
+			// This is the first test: we want to start with an empty test list file
+			if fileExists(fileName) {
+				_ = os.Remove(fileName)
+			}
+			writeProcessedTests(fileName, "# LIST OF PROCESSED TESTS")
+			writeProcessedTests(fileName, fmt.Sprintf("# VCD    : %s", strings.Replace(testConfig.Provider.Url, "/api", "", 1)))
+			writeProcessedTests(fileName, fmt.Sprintf("# version : %s", testConfig.Provider.VcdVersion))
+		}
+		if !contains(listOfProcessedTests, testName) {
+			writeProcessedTests(fileName, testName)
+			listOfProcessedTests = append(listOfProcessedTests, testName)
+		}
+		fmt.Printf("[partitioning] [%d %s (node %d)]\n", partInfo.index, testName, partitionNode)
 		if partitionDryRun {
 			t.Skipf("[DRY-RUN] partition node %d: test number %d ", partitionNode, partInfo.index)
 		}
