4550f467a65da500a720cc8ac02cc1ad64cb82b3
abarreiro <abarreiro@vmware.com>
2023-09-26 13:18:01 +0200

    Add invoke_on_refresh
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- vcd/datasource_vcd_rde_behavior_invocation.go
+++ vcd/datasource_vcd_rde_behavior_invocation.go
@@ -25,6 +25,12 @@ func datasourceVcdRdeBehaviorInvocation() *schema.Resource {
 				Required:    true,
 				Description: "The ID of either a RDE Interface Behavior or RDE Type Behavior to be invoked",
 			},
+			"invoke_on_refresh": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     true,
+				Description: "If 'true', invokes the Behavior on every refresh",
+			},
 			"arguments": {
 				Type:        schema.TypeMap,
 				Optional:    true,
@@ -49,18 +55,20 @@ func datasourceVcdRdeBehaviorInvocationRead(_ context.Context, d *schema.Resourc
 	rdeId := d.Get("rde_id").(string)
 	behaviorId := d.Get("behavior_id").(string)
 
-	rde, err := vcdClient.GetRdeById(rdeId)
-	if err != nil {
-		return diag.Errorf("[RDE Behavior Invocation] could not retrieve the RDE with ID '%s': %s", rdeId, err)
-	}
-	result, err := rde.InvokeBehavior(behaviorId, types.BehaviorInvocation{
-		Arguments: d.Get("arguments").(map[string]interface{}),
-		Metadata:  d.Get("metadata").(map[string]interface{}),
-	})
-	if err != nil {
-		return diag.Errorf("[RDE Behavior Invocation] could not invoke the Behavior of the RDE with ID '%s': %s", rdeId, err)
+	if d.Get("invoke_on_refresh").(bool) {
+		rde, err := vcdClient.GetRdeById(rdeId)
+		if err != nil {
+			return diag.Errorf("[RDE Behavior Invocation] could not retrieve the RDE with ID '%s': %s", rdeId, err)
+		}
+		result, err := rde.InvokeBehavior(behaviorId, types.BehaviorInvocation{
+			Arguments: d.Get("arguments").(map[string]interface{}),
+			Metadata:  d.Get("metadata").(map[string]interface{}),
+		})
+		if err != nil {
+			return diag.Errorf("[RDE Behavior Invocation] could not invoke the Behavior of the RDE with ID '%s': %s", rdeId, err)
+		}
+		dSet(d, "result", result)
 	}
 	d.SetId(rdeId + "|" + behaviorId) // Invocations are not real entities, so we make an artificial ID.
-	dSet(d, "result", result)
 	return nil
 }
