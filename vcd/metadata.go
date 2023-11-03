05c0161d195dbde33feb7e52ed7d4d6eb5a072ed
abarreiro <abarreiro@vmware.com>
2023-11-03 12:14:24 +0100

    Apply suggestion
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- vcd/metadata.go
+++ vcd/metadata.go
@@ -386,7 +386,8 @@ func updateMetadataInState(d *schema.ResourceData, vcdClient *VCDClient, resourc
 
 // filterAndGetVcdInheritedMetadata filters out the metadata entries that were created automatically by VCD (inherited metadata)
 // from the input metadata parameter, then returns these entries as a structure that is ready to be set in the
-// Terraform schema. The filtered out entries will not be longer present in the original metadata input parameter.
+// Terraform schema.
+// Note: the filtered out entries are removed from the original metadata input parameter.
 func filterAndGetVcdInheritedMetadata(metadata *types.Metadata) []interface{} {
 	if len(metadata.MetadataEntry) == 0 {
 		return nil
