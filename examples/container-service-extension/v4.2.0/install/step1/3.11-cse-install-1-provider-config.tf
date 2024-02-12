50b20cc3956f5dcd64da0ed42b26103b42ca705f
abarreiro <abarreiro@vmware.com>
2024-02-12 11:43:01 +0100

    Add 4.2.0
    Signed-off-by: abarreiro <abarreiro@vmware.com>

---
+++ examples/container-service-extension/v4.2.0/install/step1/3.11-cse-install-1-provider-config.tf
@@ -0,0 +1,34 @@
+# ------------------------------------------------------------------------------------------------------------
+# CSE 4.2.0 installation, step 1:
+#
+# * Please read the guide present at https://registry.terraform.io/providers/vmware/vcd/latest/docs/guides/container_service_extension_4_x_install
+#   before applying this configuration.
+#
+# * The installation process is split into two steps as the first one creates a CSE admin user that needs to be
+#   used in a "provider" block in the second one.
+#
+# * Rename "terraform.tfvars.example" to "terraform.tfvars" and adapt the values to your needs.
+#   Other than that, this snippet should be applied as it is.
+# ------------------------------------------------------------------------------------------------------------
+
+# VCD Provider configuration. It must be at least v3.11.0 and configured with a System administrator account.
+terraform {
+  required_providers {
+    vcd = {
+      source  = "vmware/vcd"
+      version = ">= 3.11"
+    }
+  }
+}
+
+provider "vcd" {
+  url                  = "${var.vcd_url}/api"
+  user                 = var.administrator_user
+  password             = var.administrator_password
+  auth_type            = "integrated"
+  sysorg               = var.administrator_org
+  org                  = var.administrator_org
+  allow_unverified_ssl = var.insecure_login
+  logging              = true
+  logging_file         = "cse_install_step1.log"
+}
