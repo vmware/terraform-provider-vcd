9a443f6b8aaab617d7a5bd58cdc0351d2c0ae705
abarreiro <abarreiro@vmware.com>
2024-04-30 11:55:01 +0200

    Checkpoint, resource almost finished
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- vcd/resource_vcd_org_oidc.go
+++ vcd/resource_vcd_org_oidc.go
@@ -8,7 +8,9 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/vmware/go-vcloud-director/v2/govcd"
+	"github.com/vmware/go-vcloud-director/v2/types/v56"
 	"log"
+	"os"
 )
 
 // resourceVcdOrgOidc defines the resource that manages Open ID Connect (OIDC) settings for an existing Organization
@@ -36,6 +38,7 @@ func resourceVcdOrgOidc() *schema.Resource {
 			"client_secret": {
 				Type:        schema.TypeString,
 				Required:    true,
+				Sensitive:   true,
 				Description: "Client Secret to use when talking to the OIDC Identity Provider",
 			},
 			"enabled": {
@@ -49,59 +52,105 @@ func resourceVcdOrgOidc() *schema.Resource {
 				Description: "Endpoint from the OIDC Identity Provider that serves all the configuration values",
 			},
 			"issuer_id": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true, // Can be obtained with "wellknown_endpoint"
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true, // Can be obtained with "wellknown_endpoint"
+				Description:  "If 'wellknown_endpoint' is set, this attribute overrides the obtained Issuer ID",
+				AtLeastOneOf: []string{"issuer_id", "wellknown_endpoint"},
+			},
+			"prefer_id_token": {
+				Type:        schema.TypeBool,
+				Required:    true,
 				Description: "",
 			},
 			"user_authorization_endpoint": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true, // Can be obtained with "wellknown_endpoint"
-				Description: "",
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true, // Can be obtained with "wellknown_endpoint"
+				Description:  "If 'wellknown_endpoint' is set, this attribute overrides the obtained User Authorization endpoint",
+				AtLeastOneOf: []string{"user_authorization_endpoint", "wellknown_endpoint"},
 			},
-			"access_endpoint_endpoint": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true, // Can be obtained with "wellknown_endpoint"
-				Description: "",
+			"access_token_endpoint": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true, // Can be obtained with "wellknown_endpoint"
+				Description:  "If 'wellknown_endpoint' is set, this attribute overrides the obtained Access Token endpoint",
+				AtLeastOneOf: []string{"access_token_endpoint", "wellknown_endpoint"},
 			},
 			"userinfo_endpoint_endpoint": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true, // Can be obtained with "wellknown_endpoint"
-				Description: "",
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true, // Can be obtained with "wellknown_endpoint"
+				Description:  "If 'wellknown_endpoint' is set, this attribute overrides the obtained Userinfo endpoint",
+				AtLeastOneOf: []string{"userinfo_endpoint_endpoint", "wellknown_endpoint"},
 			},
 			"max_clock_skew": {
 				Type:             schema.TypeInt,
-				Optional:         true,
-				Computed:         true, // Can be obtained with "wellknown_endpoint"
+				Required:         true,
 				Description:      "",
 				ValidateDiagFunc: minimumValue(0, "'max_clock_skew' must be higher than or equal to 0"),
 			},
 			"scopes": {
-				Type: schema.TypeSet,
+				Type:     schema.TypeSet,
+				MinItems: 1,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
 				},
 				Optional:    true,
 				Computed:    true, // Can be obtained with "wellknown_endpoint"
-				Description: "",
+				Description: "If 'wellknown_endpoint' is set, this attribute overrides the obtained Scopes",
 			},
 			"claims_mapping": {
-				Type:        schema.TypeMap,
+				Type:        schema.TypeList,
+				MaxItems:    1,
 				Optional:    true,
 				Computed:    true, // Can be obtained with "wellknown_endpoint"
-				Description: "",
-				ValidateDiagFunc: func(i interface{}, path cty.Path) diag.Diagnostics {
-					// Check that keys match only 'subject', 'last_name', etc
-					return nil
+				Description: "If 'wellknown_endpoint' is set, this attribute overrides the obtained Claim mappings",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"email": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "",
+						},
+						"subject": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "",
+						},
+						"last_name": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "",
+						},
+						"first_name": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "",
+						},
+						"full_name": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "",
+						},
+						"groups": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "",
+						},
+						"roles": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "",
+						},
+					},
 				},
 			},
 			"key": {
 				Type:        schema.TypeSet,
 				MinItems:    1,
-				Required:    true, // FIXME: Can be obtained with "wellknown_endpoint"
+				Optional:    true,
+				Computed:    true, // Can be obtained with "wellknown_endpoint"
 				Description: "",
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
@@ -110,33 +159,63 @@ func resourceVcdOrgOidc() *schema.Resource {
 							Required:    true,
 							Description: "",
 						},
+						"algorithm": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "",
+						},
 						"pem_file": {
-							Type:     schema.TypeString,
-							Required: true,
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "",
 						},
 						"expiration": {
-							Type:     schema.TypeString,
-							Optional: true,
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "",
+						},
+						"pem": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "",
 						},
 					},
 				},
 			},
 			"key_refresh_endpoint": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Computed:    true, // Can be obtained with "wellknown_endpoint"
-				Description: "",
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true, // Can be obtained with "wellknown_endpoint"
+				Description:  "",
+				RequiredWith: []string{"key_refresh_period", "key_refresh_strategy"},
 			},
-			"key_refresh_period": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "",
+			"key_refresh_period_hours": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Description:  "",
+				RequiredWith: []string{"key_refresh_endpoint", "key_refresh_strategy"},
+				ValidateDiagFunc: func(i interface{}, path cty.Path) diag.Diagnostics {
+					return nil
+					// Maximum 30 days: 24*30
+				},
 			},
 			"key_refresh_strategy": {
 				Type:         schema.TypeString,
 				Optional:     true,
 				Description:  "",
-				ValidateFunc: validation.StringInSlice([]string{"add", "replace", "expire_after"}, false),
+				ValidateFunc: validation.StringInSlice([]string{"ADD", "REPLACE", "EXPIRE_AFTER"}, false),
+				RequiredWith: []string{"key_refresh_endpoint", "key_refresh_period"},
+			},
+			"key_expire_duration_hours": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Description:  "",
+				RequiredWith: []string{"key_refresh_strategy"},
+				ValidateDiagFunc: func(i interface{}, path cty.Path) diag.Diagnostics {
+					return nil
+					// Only if key_refresh_strategy=EXPIRE_AFTER
+					// Maximum 1 days: 24
+				},
 			},
 			"ui_button_label": {
 				Type:        schema.TypeString,
@@ -157,10 +236,73 @@ func resourceVcdOrgOidcCreateOrUpdate(ctx context.Context, d *schema.ResourceDat
 
 	orgId := d.Get("org_id").(string)
 
-	_, err := vcdClient.GetAdminOrgById(orgId)
+	org, err := vcdClient.GetAdminOrgById(orgId)
 	if err != nil {
 		return diag.Errorf("[Organization Open ID Connect %s] error searching for Org '%s': %s", operation, orgId, err)
 	}
+
+	isWellKnownEndpointUsed := d.Get("wellknown_endpoint").(string) != ""
+
+	settings := types.OrgOAuthSettings{
+		IssuerId:                   d.Get("issuer_id").(string),
+		Enabled:                    d.Get("enabled").(bool),
+		ClientId:                   d.Get("client_id").(string),
+		ClientSecret:               d.Get("client_secret").(string),
+		UserAuthorizationEndpoint:  d.Get("user_authorization_endpoint").(string),
+		AccessTokenEndpoint:        d.Get("access_token_endpoint").(string),
+		UserInfoEndpoint:           d.Get("userinfo_endpoint").(string),
+		MaxClockSkew:               d.Get("max_clock_skew").(int),
+		JwksUri:                    d.Get("key_refresh_endpoint").(string),
+		AutoRefreshKey:             d.Get("key_refresh_endpoint").(string) != "" && d.Get("key_refresh_strategy").(string) != "",
+		KeyRefreshStrategy:         d.Get("key_refresh_strategy").(string),
+		KeyRefreshFrequencyInHours: d.Get("key_refresh_period_hours").(int),
+		WellKnownEndpoint:          d.Get("wellknown_endpoint").(string),
+		EnableIdTokenClaims:        d.Get("prefer_id_token").(bool),
+		CustomUiButtonLabel:        d.Get("ui_button_label").(string),
+		Scope:                      convertTypeListToSliceOfStrings(d.Get("scopes").(*schema.Set).List()),
+		// TODO UsePKCE:                    false,
+		// TODO SendClientCredentialsAsAuthorizationHeader: false,
+	}
+
+	// Key configurations: OAuthKeyConfigurations
+	keyList := d.Get("key").(*schema.Set).List()
+	oAuthKeyConfigurations := make([]*types.OAuthKeyConfiguration, len(keyList))
+	for i, k := range keyList {
+		key := k.(map[string]interface{})
+		oAuthKeyConfigurations[i] = &types.OAuthKeyConfiguration{
+			KeyId:          key["id"].(string),
+			Algorithm:      key["algorithm"].(string),
+			ExpirationDate: key["expiration_date"].(string), // FIXME: Formatting???
+		}
+		pemContents, err := os.ReadFile(key["pem_file"].(string))
+		if err != nil {
+			return diag.Errorf("[Organization Open ID Connect %s] error reading PEM file '%s': %s", operation, key["pem_file"].(string), err)
+		}
+		oAuthKeyConfigurations[i].Key = string(pemContents)
+	}
+
+	// Claims mapping: OIDCAttributeMapping: Subject, Email, Full name, First name and Last name are mandatory
+	claimsMapping := d.Get("claims_mapping").([]map[string]interface{})
+	if len(claimsMapping) == 0 && !isWellKnownEndpointUsed {
+		return diag.Errorf("[Organization Open ID Connect %s] error reading claims, either set a 'claims_mapping' block or set 'wellknown_endpoint' to obtain this information", operation)
+	}
+	if len(claimsMapping) > 0 {
+		var oidcAttributeMapping types.OIDCAttributeMapping
+		oidcAttributeMapping.SubjectAttributeName = claimsMapping[0]["subject"].(string)
+		oidcAttributeMapping.EmailAttributeName = claimsMapping[0]["email"].(string)
+		oidcAttributeMapping.FullNameAttributeName = claimsMapping[0]["full_name"].(string)
+		oidcAttributeMapping.FirstNameAttributeName = claimsMapping[0]["first_name"].(string)
+		oidcAttributeMapping.LastNameAttributeName = claimsMapping[0]["last_name"].(string)
+		oidcAttributeMapping.GroupsAttributeName = claimsMapping[0]["groups"].(string)
+		oidcAttributeMapping.RolesAttributeName = claimsMapping[0]["roles"].(string)
+		settings.OIDCAttributeMapping = &oidcAttributeMapping
+	}
+
+	_, err = org.SetOpenIdConnectSettings(settings)
+	if err != nil {
+		return diag.Errorf("[Organization Open ID Connect create] Could not set OIDC settings: %s", err)
+	}
+
 	return resourceVcdOrgOidcRead(ctx, d, meta)
 }
 func resourceVcdOrgOidcCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
@@ -194,6 +336,55 @@ func genericVcdOrgOidcRead(_ context.Context, d *schema.ResourceData, meta inter
 	}
 
 	dSet(d, "client_id", settings.ClientId)
+	dSet(d, "client_secret", settings.ClientSecret)
+	dSet(d, "enabled", settings.Enabled)
+	dSet(d, "wellknown_endpoint", settings.WellKnownEndpoint)
+	dSet(d, "issuer_id", settings.IssuerId)
+	dSet(d, "prefer_id_token", settings.EnableIdTokenClaims)
+	dSet(d, "user_authorization_endpoint", settings.UserAuthorizationEndpoint)
+	dSet(d, "access_token_endpoint", settings.AccessTokenEndpoint)
+	dSet(d, "userinfo_endpoint_endpoint", settings.UserInfoEndpoint)
+	dSet(d, "max_clock_skew", settings.MaxClockSkew)
+	err = d.Set("scopes", settings.Scope)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	if settings.OIDCAttributeMapping != nil {
+		claims := map[string]string{}
+		claims["email"] = settings.OIDCAttributeMapping.EmailAttributeName
+		claims["subject"] = settings.OIDCAttributeMapping.SubjectAttributeName
+		claims["last_name"] = settings.OIDCAttributeMapping.LastNameAttributeName
+		claims["first_name"] = settings.OIDCAttributeMapping.FirstNameAttributeName
+		claims["full_name"] = settings.OIDCAttributeMapping.FullNameAttributeName
+		claims["groups"] = settings.OIDCAttributeMapping.GroupsAttributeName
+		claims["roles"] = settings.OIDCAttributeMapping.RolesAttributeName
+		err = d.Set("claims_mapping", claims)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	if settings.OAuthKeyConfigurations != nil {
+		keyConfigurations := settings.OAuthKeyConfigurations.OAuthKeyConfiguration
+		keyConfigs := make([]map[string]interface{}, len(keyConfigurations))
+		for i, keyConfig := range keyConfigurations {
+			key := map[string]interface{}{}
+			key["id"] = keyConfig.KeyId
+			key["algorithm"] = keyConfig.Algorithm
+			key["pem"] = keyConfig.Key
+			key["expiration"] = keyConfig.ExpirationDate
+			keyConfigs[i] = key
+		}
+		err = d.Set("key", keyConfigs)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	dSet(d, "key_refresh_endpoint", settings.JwksUri)
+	dSet(d, "key_refresh_period_hours", settings.KeyRefreshFrequencyInHours)
+	dSet(d, "key_refresh_strategy", settings.KeyRefreshStrategy)
+	dSet(d, "key_expire_duration_hours", settings.KeyExpireDurationInHours)
+	dSet(d, "ui_button_label", settings.CustomUiButtonLabel)
+	dSet(d, "redirect_uri", settings.OrgRedirectUri)
 	d.SetId(adminOrg.AdminOrg.ID)
 
 	return nil
@@ -203,9 +394,14 @@ func resourceVcdOrgOidcDelete(_ context.Context, d *schema.ResourceData, meta in
 	vcdClient := meta.(*VCDClient)
 	orgId := d.Get("org_id").(string)
 
-	_, err := vcdClient.GetAdminOrgById(orgId)
+	adminOrg, err := vcdClient.GetAdminOrgById(orgId)
 	if err != nil {
 		return diag.Errorf("[Organization Open ID Connect delete] error searching for Organization '%s': %s", orgId, err)
+	}
+
+	err = adminOrg.DeleteOpenIdConnectSettings()
+	if err != nil {
+		return diag.Errorf("[Organization Open ID Connect delete] error deleting OIDC settings for Organization '%s': %s", orgId, err)
 	}
 
 	return nil
