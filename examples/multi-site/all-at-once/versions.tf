eb537f2d5d1e0d19b8a49d6d8857de19cac57b31
Giuseppe Maxia <giuseppe.maxia@broadcom.com>
2024-05-09 15:27:11 +0200

    [skip-ci] Update examples version
    Signed-off-by: Giuseppe Maxia <giuseppe.maxia@broadcom.com>

--- examples/multi-site/all-at-once/versions.tf
+++ examples/multi-site/all-at-once/versions.tf
@@ -2,9 +2,8 @@ terraform {
   required_providers {
     vcd = {
       source  = "vmware/vcd"
-      version = ">=3.12.0"
+      version = ">=3.13.0"
     }
   }
-  # required_version = ">= 0.13"
 }
 
