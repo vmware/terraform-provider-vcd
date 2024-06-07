4fdfbc09a144294112c2282906f2897c1cfd70bb
Dainius Serplis <dserplis@vmware.com>
2024-06-07 15:10:56 +0300

    skip test for Org user
    Signed-off-by: Dainius Serplis <dserplis@vmware.com>

--- vcd/resource_vcd_solution_add_on_test.go
+++ vcd/resource_vcd_solution_add_on_test.go
@@ -14,6 +14,8 @@ import (
 
 func TestAccSolutionAddon(t *testing.T) {
 	preTestChecks(t)
+	skipIfNotSysAdmin(t)
+
 	if checkVersion(testConfig.Provider.ApiVersion, "< 37.1") {
 		t.Skipf("Solution Landing Zones are supported in VCD 10.4.1+. Skipping")
 	}
