45b4793cbf80de6dd02813a20d3cdbac0ff8ad5b
abarreiro <abarreiro@vmware.com>
2024-08-29 13:26:01 +0200

    Fix
    Signed-off-by: abarreiro <abarreiro@vmware.com>

--- vcd/resource_vcd_vapp_vm_tools.go
+++ vcd/resource_vcd_vapp_vm_tools.go
@@ -695,7 +695,7 @@ func updateTemplateInternalDisks(d *schema.ResourceData, meta interface{}, vm go
 		}
 
 		// Update details of internal disk for disk existing in template
-		if value, ok := internalDiskProvidedConfig["iops"]; ok {
+		if value, ok := internalDiskProvidedConfig["iops"]; ok && diskCreatedByTemplate.IopsAllocation != nil {
 			iops := int64(value.(int))
 			diskCreatedByTemplate.IopsAllocation.Reservation = iops
 		}
